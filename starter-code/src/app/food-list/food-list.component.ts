279ba6d5cc0b9a961056a4d6579bfb6d4da1cec9
Darek Glowinski <dariushg32111@gmail.com>
2017-05-16 15:42:31 +0200

    finish

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import foods from '../foods';
+import foodsList from '../foods';
 
 @Component({
   selector: 'app-food-list',
@@ -7,10 +7,42 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
+  foods: any[];
+  todayFoods: any[] = [];
+  newFood: any ={};  
+  pattern: string;
+  formClass:string="hide";
+  totalCalories: number = 0;
+  quantities:number[] = [];
+  
+  addToday(index, quantity) {
+    let food = this.foods[index];
+    
+    let ind = this.todayFoods.findIndex(f=>food.name===f.name);
 
+    if(ind!==-1) {
+      this.todayFoods[ind].quantity+= parseInt(quantity);
+    } else {
+      food.quantity = parseInt(quantity);
+      this.todayFoods.push(food);
+    }
+    this.totalCalories = 0;
+    this.foods.forEach(f=>this.totalCalories+=f.quantity*f.calories)
+  }
+
+  addFood(food) {
+    this.foods.push(food);
+    this.toggleShowAddFood();
+  }
+
+  toggleShowAddFood() {
+    this.formClass = this.formClass ? "" : "hide";
+  }
   constructor() { }
 
   ngOnInit() {
+    this.foods = foodsList;
+    this.foods.forEach((food, index)=>this.quantities[index]=1)
   }
 
 }
