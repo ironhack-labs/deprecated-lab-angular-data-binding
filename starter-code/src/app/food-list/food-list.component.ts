6678c97f2b5aa5f599a94c9b4f86151778a79819
Ryan Jones <ryanjones@Ryans-MacBook.local>
2017-05-16 19:45:51 +0200

    complete

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Pipe } from '@angular/core';
 import foods from '../foods';
+
+
 
 @Component({
   selector: 'app-food-list',
@@ -7,10 +9,35 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
-
+  foods: Object[];
+  newFood: Object = {};
+  todayFood: Array<string> = [];
+  newTodayFood: Object = {};
+  showform: boolean = false;
   constructor() { }
 
   ngOnInit() {
+    this.foods = foods;
   }
 
+
+  addFood(){
+    if (!this.showform){
+      this.showform = true;
+    } else{
+      this.showform = false;
+    }
+  }
+
+  createFood(){
+    this.foods.push(this.newFood);
+    this.newFood= "";
+  }
+
+  todaysFood(food){
+    console.log(food)
+    this.todayFood.push(food);
+  }
+
+  
 }
