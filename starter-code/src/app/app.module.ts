96f12f2f7b29e9bbeff176b1f9c28a6810ca9a0d
Carlos Luis <clburguete@gmail.com>
2017-05-16 18:59:11 +0200

    iteration2

--- starter-code/src/app/app.module.ts
+++ starter-code/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { FoodListComponent } from './food-list/food-list.component';
+import { FilterPipe } from './pipes/filter.pipe';
+import { CustomPipesComponent } from './custom-pipes/custom-pipes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FoodListComponent,
+    FilterPipe,
+    CustomPipesComponent,
   ],
   imports: [
     BrowserModule,
