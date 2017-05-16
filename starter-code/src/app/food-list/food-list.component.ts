bfd147d7771fe19bcc44c6d2ca1f68b89beccfa4
Giuseppe Fierro <fierro.gpp@gmail.com>
2017-05-16 17:53:10 +0200

    done

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -7,10 +7,57 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
-
+  foods: any[];
+  newFood:any =  {};
+  show: boolean = false;
+  todaysList: any[] = [];
+  totalCalories: number = 0;
+  quantity: number = 1;
   constructor() { }
 
   ngOnInit() {
+    this.foods = foods;
   }
 
+  showForm(){
+    this.show = this.show ? false : true;
+  }
+  addFood(){
+    console.log("Add contact has been called");
+
+    this.newFood["quantity"] = 1;
+    this.foods.push(this.newFood);
+    this.show = false;
+  }
+
+  addTodayFood(food){
+    console.log(food.quantity);
+    var found = 0;
+
+    if(food.quantity > 0){
+
+      if(this.todaysList.length>0){
+      this.todaysList.forEach((today)=> {
+        console.log("today's name", today["name"]);
+        console.log("food's name", food.name);
+        if(today["name"] == food.name){
+          today["quantity"]= food.quantity;
+          found = 1;
+        }
+
+      });
+      if(!found) this.todaysList.push(food);
+      this.totalCalories+=food.calories * food.quantity;
+    } else {
+      this.todaysList.push(food);
+      this.totalCalories+=food.calories * food.quantity;
+    }
+    }
+
+    }
+
+
+  updateQuantity(i){
+    this.foods[i]["quantity"]+= this.quantity;
+  }
 }
