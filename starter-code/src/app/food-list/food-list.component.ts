4e924f73b19a498c70df3879cf82d75159e9d372
Alejandro <ajimenez1724@gmail.com>
2017-06-22 20:56:48 -0400

    iteration 3

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -7,13 +7,54 @@ import foodList from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
-  foods: Object[];
-  pattern: String;
+  foods          : Object[];
+   myList         : {name: string, calories: number, quantity: number, image: string}[] = [];
+   pattern        : string;
+   isEditing      : boolean = false;
+   newFoodName    : string = "Example Name";
+   newFoodCalories: number = 250;
+   newFoodImage   : string = "http://dhiglobal.com/wp-content/uploads/2016/07/placeholder.jpg";
+   quantity       : number;
+   totalCalories  : number = 0;
 
   constructor() { }
 
   ngOnInit() {
     this.foods = foodList;
+  }
+
+   enableUserAddFood(){
+    this.isEditing = !this.isEditing;
+  }
+
+  newFood(){
+    const newFood = {
+      name: this.newFoodName,
+      calories: this.newFoodCalories,
+      image: this.newFoodImage,
+      quantity: 0
+    }
+
+    this.foods.unshift(newFood);
+
+    this.isEditing = false;
+    this.newFoodName = "";
+    this.newFoodCalories = null;
+    this.newFoodImage = "";
+  }
+
+  addToMyList(food, quantityInput){
+    const existingFood = this.myList.find(item => item.name === food.name)
+    const quantity = Number(quantityInput.value)
+
+    if (existingFood){
+      existingFood.quantity += quantity;
+    } else {
+      food.quantity = quantity;
+      this.myList.push(food);
+    }
+    this.totalCalories += (food.calories * quantity);
+    this.quantity = 1;
   }
 
 }
