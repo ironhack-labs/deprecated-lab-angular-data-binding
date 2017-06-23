aa0a79d9b1f3164a34abd7e96c0dba4dee70aa86
Alejandro <ajimenez1724@gmail.com>
2017-06-22 20:03:12 -0400

    iteration 2

--- starter-code/src/app/app.module.ts
+++ starter-code/src/app/app.module.ts
@@ -3,13 +3,20 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+
+
+
 import { AppComponent } from './app.component';
 import { FoodListComponent } from './food-list/food-list.component';
+import { FoodSearchPipe } from './food-search-pipe';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     FoodListComponent,
+    FoodSearchPipe
   ],
   imports: [
     BrowserModule,
