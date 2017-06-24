438792f9a9943076b709dd79c2df915eb4cd4b9d
ogm710811 <ogm710811@gmail.com>
2017-06-24 02:05:51 -0400

    super bonus done

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -18,7 +18,8 @@ export class FoodListComponent implements OnInit {
   todayFood : Object = {
     name: '',
     calories: 0,
-    quantity: 0
+    quantity: 0,
+    amountClicked: 0
   }
 
   todayFoodList: Object[] = [];
@@ -41,28 +42,44 @@ export class FoodListComponent implements OnInit {
     this.todayFood = { 
       name : food['name'],
       calories : food['calories'],
-      quantity: quantity.value
+      quantity: quantity.value,
+      amountClicked: 1
     }
     
-    this.todayFoodList.push(this.todayFood);  
-    this.todayFoodList.forEach( oneFood => {
-      console.log(oneFood['calories']);
-    })
+    // first check if todayFoodList is empty if so, push first food selected
+    if ( this.todayFoodList.length === 0 ) {
+      this.todayFoodList.push(this.todayFood);
+      this.caloriesTodayList = getTotalCalories(this.todayFoodList);
 
-  /*********************************************************************************/
-  /* Getting the total calories of the today list of food
-  /* using map and reduce JavaScript functions in array of Object
-  /* map() - dealing only with the values of categories key in the object
-  /* reduce() - totaling what mao() returns
-  /*********************************************************************************/
-    let totalCaloriesTodayList = this.todayFoodList.map((item) => {
-      return item['calories'];
-    })
-    this.caloriesTodayList = totalCaloriesTodayList.reduce((previous, current) => {
-      return previous + current;
-    })
-    console.log(`Total Calories Today Food => ${ totalCaloriesTodayList }`);
-    console.log(`Total Calories Today Food => ${ this.caloriesTodayList }`);
+      this.todayFoodList.forEach((oneFood) => {
+        console.log(oneFood['amountClicked']);
+      })
+
+      // if todayFoodList has at least one element then check if food already exists
+      // if food exists doesn't push it again, so updated the amountClicked value,
+      // and updated the number of calories multiplying by the amountClicked value.
+    } else {
+      console.log(checkDuplicateFood(this.todayFoodList, this.todayFood['name']))
+      if (checkDuplicateFood(this.todayFoodList, this.todayFood['name'])) {
+        this.todayFoodList.forEach((oneFood) => {
+          if (oneFood['name'] === this.todayFood['name']) {
+            oneFood['amountClicked']++;
+            console.log(oneFood['amountClicked'])
+
+            // this is to updated calories if the food is clicked more than one
+            // I will comment out because is not a requirement
+            // oneFood['calories'] = oneFood['calories'] * oneFood['amountClicked'];
+            // oneFood['quantity']++;
+            // this.caloriesTodayList = getTotalCalories(this.todayFoodList);
+          }
+        })
+      } else {
+        this.todayFoodList.push(this.todayFood);
+        this.caloriesTodayList = getTotalCalories(this.todayFoodList);
+      }
+    }
+    
+    
   }
 
   /*********************************************************************************/
@@ -79,8 +96,37 @@ export class FoodListComponent implements OnInit {
     this.foods = foodList;
     this.tFoods = this.todayFoodList;
   }
+}// FoodListComponent ends
 
+  /*********************************************************************************/
+  /* getTotalCalories( todayFoodList : Object[] )
+  /* Getting the total calories of the today list of food
+  /* using map and reduce JavaScript functions in array of Object
+  /* map() - dealing only with the values of categories key in the object
+  /* reduce() - totaling what mao() returns
+  /*********************************************************************************/
+function getTotalCalories( todayFoodList : Object[] ) : number{
+
+  let totalCaloriesTodayList = todayFoodList.map((item) => {
+    return item['calories'];
+  })
   
-  
-  
+  let caloriesTodayList = totalCaloriesTodayList.reduce((previous, current) => {
+    return previous + current;
+  })
+
+  console.log(`Total Calories Today Food => ${ totalCaloriesTodayList }`);
+  console.log(`Total Calories Today Food => ${ caloriesTodayList }`);
+
+  return caloriesTodayList;
+}
+
+/*********************************************************************************/
+  /* checkDuplicateFood(arr, val)
+  /* checks if the food added already exists in the array of today food list
+  /*******************************************************************************/
+function checkDuplicateFood(arr, val) {
+  return arr.some(function(e) {
+        return e.name === val;
+  });
 }
