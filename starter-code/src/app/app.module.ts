7ffefe4c0b98107f2a5d7192da18575763ec492d
Nik E <NikolasEchenique@gmail.com>
2017-07-14 13:35:16 -0400

    done

--- starter-code/src/app/app.module.ts
+++ starter-code/src/app/app.module.ts
@@ -5,11 +5,17 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { FoodListComponent } from './food-list/food-list.component';
+import { DoTheSearchPipe } from './do-the-search.pipe';
+import { AllFoodListComponent } from './food-list/all-food-list/all-food-list.component';
+import { TodayFoodListComponent } from './food-list/today-food-list/today-food-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FoodListComponent,
+    DoTheSearchPipe,
+    AllFoodListComponent,
+    TodayFoodListComponent,
   ],
   imports: [
     BrowserModule,
--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -7,10 +7,47 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
+    newFood: any = {};
+    foodList: Object[];
+    isShown: boolean = true;
+  todayFoodList: Array<Object> = [];
+  totalCalories: number = 0;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+    this.foodList = foods;
+
+  }
+  addFood(){
+    console.log("Add contact has been called");
+    console.log(this.newFood);
+    this.foodList.push(this.newFood);
+
+    this.newFood = {};
+
+  }
+
+  buttonA(){
+    if(this.isShown === true){
+          this.isShown = false;
+      }else{
+        this.isShown = true;
+      }
+    }
+
+    
+
+  foodToday(theFood){
+    if(theFood === this.newFood){
+      this.totalCalories += this.newFood.calories;
+    }else{
+      this.totalCalories += theFood.calories;
+    }
+
+    const foodIndex = this.foodList.indexOf(theFood);
+    this.todayFoodList.push(theFood)
   }
 
 }
