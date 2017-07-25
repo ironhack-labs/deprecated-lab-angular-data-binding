4cb4847c2d4c2d2b7957d033e0c05b32153917f5
Marc Palatsí <mpalatsi@me.com>
2017-07-25 15:47:37 +0200

    iteration 1 done

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -8,9 +8,13 @@ import foods from '../foods';
 })
 export class FoodListComponent implements OnInit {
 
+  foods: Object[];
+  newFood: Object = {}
+
   constructor() { }
 
   ngOnInit() {
+    this.foods = foods;
   }
 
 }
