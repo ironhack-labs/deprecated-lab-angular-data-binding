0370aeb6c6230e2c5284ed339fe0fc3f60565936
mafevito <mafevito@hotmail.com>
2017-07-25 19:22:21 +0200

    Done Iteration4

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -8,11 +8,43 @@ import foods from '../foods';
 })
 export class FoodListComponent implements OnInit {
   foods: Object[];
+  newFood: Object = {};
+  hidden: boolean = false;
+  todaysFood: any = []
 
-  constructor() { }
+  showList:number;
+
+  constructor() {
+     this.foods = foods;
+   }
 
   ngOnInit() {
-    this.foods = foods;
+     this.foods = foods;
   }
 
+  addForm() {
+    this.hidden = !this.hidden;
+  }
+
+  addFood() {
+    console.log("Add contact has been called");
+    this.foods.push(this.newFood);
+    this.newFood = {};
+    this.hidden = !this.hidden;
+  }
+
+  addList(food) {
+    this.todaysFood.push(food)
+    console.log(this.todaysFood)
+    this.totalCalories(this.todaysFood);
+  }
+
+  totalCalories(todaysFood) {
+    let total = 0
+    for (let i = 0; i < todaysFood.length; i++) {
+       total += todaysFood[i].calories
+     }
+    this.showList = total
+    return total
+  }
 }
