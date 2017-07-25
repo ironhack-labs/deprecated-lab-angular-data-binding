89062780f5bd1f0c42ba9162c8d9f073b26e633d
teresatorrijos <torrijosgarcia.teresa@gmail.com>
2017-07-25 17:43:45 +0200

    second iteration

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -6,11 +6,25 @@ import foods from '../foods';
   templateUrl: './food-list.component.html',
   styleUrls: ['./food-list.component.css']
 })
-export class FoodListComponent implements OnInit {
 
-  constructor() { }
+export class FoodListComponent implements OnInit {
+  foods: Object[];
+  newFood: Object = {};
+  constructor() {
+    this.foods = foods;
+    }
 
   ngOnInit() {
+    this.foods = foods;
   }
 
 }
+
+
+  // addContact(){
+  //   console.log("Add contact has been called");
+  //   // add contact to contacts list
+  //   // clear input
+  //   this.contacts.push(this.newContact);
+  //   this.newContact = {};
+  // }
