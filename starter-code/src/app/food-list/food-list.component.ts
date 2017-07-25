9592cc80603785f4ceea5cafd1bc6550d668f336
Mario Martin <mariomarsan7@gmail.com>
2017-07-25 18:32:05 +0200

    Iteration 3 add Food DONE

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -1,29 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 import foodList from '../foods';
 
+interface FoodItem {
+  name: string,
+  calories: number,
+  image: string,
+  quantity: number,
+}
+
+class Food implements FoodItem{
+  quantity: number;
+  constructor(public name:string, public calories: number, public image:string){
+    this.quantity = 0;
+  }
+}
+
 @Component({
   selector: 'app-food-list',
   templateUrl: './food-list.component.html',
   styleUrls: ['./food-list.component.css']
 })
+
 export class FoodListComponent implements OnInit {
 
-  foods: Array<Object> = [];
+  foods: Object[];
   newFood: Object = {};
-  // pattern: string;
+  errorMessage = false;
+  //pattern: string;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     this.foods = foodList;
   }
 
-  addContact(){
-    console.log("Add contact has been called");
-    // add food to foods list
-    this.foods.push(this.newFood);
-    // clear inputs
-    this.newFood = {};
+  addFood(food){
+    if(food.name !== undefined && food.calories !== undefined && food.image !== undefined){
+      food = new Food(food.name,food.calories,food.image);
+      this.foods.push(food);  // add food to foods list
+       // clear inputs
+      this.newFood = {};
+    }else{
+      this.errorMessage = true;
+    }
   }
 
 }
