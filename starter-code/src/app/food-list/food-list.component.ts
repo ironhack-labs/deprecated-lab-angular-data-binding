d4c4bf7be4db4d216f31c6e88b297aa9097cbd53
rosadl <rosadl84@gmail.com>
2017-07-25 18:56:15 +0200

    iteration 3

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import foods from '../foods';
+import foodList from '../foods';
 
 @Component({
   selector: 'app-food-list',
@@ -7,10 +7,24 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
-
+foods : Object[];
+newFood: Object = {};
+pattern: String;
+show: boolean = false;
   constructor() { }
 
   ngOnInit() {
+     this.foods = foodList;
+  }
+  toggleShow(){
+    this.show = !this.show;
+  }
+  addFood(){
+    console.log("Add food has been called");
+    // add contact to contacts list
+    this.foods.push(this.newFood)
+    this.newFood = {}
+    this.show = !this.show;
   }
 
 }
