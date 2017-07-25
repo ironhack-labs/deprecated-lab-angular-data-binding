274cdca0da6321a2b5c0e0dad70a0a8bad71649b
Fernando Palma <ferpalma21@github.com>
2017-07-25 17:16:06 +0200

    1st iteration

--- starter-code/src/app/app.module.ts
+++ starter-code/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { FoodListComponent } from './food-list/food-list.component';
+import { CustomPipesComponent } from './custom-pipes/custom-pipes.component';
+import { FoodPipesComponent } from './food-pipes/food-pipes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FoodListComponent,
+    CustomPipesComponent,
+    FoodPipesComponent,
   ],
   imports: [
     BrowserModule,
--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import foods from '../foods';
 
+
 @Component({
   selector: 'app-food-list',
   templateUrl: './food-list.component.html',
   styleUrls: ['./food-list.component.css']
 })
-export class FoodListComponent implements OnInit {
 
+export class FoodListComponent implements OnInit {
+  foodList : Array<Object>;
+  newFood : Object = {};
   constructor() { }
 
   ngOnInit() {
+    this.foodList = foods;
   }
 
 }
