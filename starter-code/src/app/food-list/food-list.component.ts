3f62981b66d0657101da39af9449ec468a43aa4f
Juan Macias <yo@soyjuanmacias.com>
2017-07-25 21:20:45 +0200

    Croquetas de la casa, jamon, ensaladilla, pimientos rellenos... Aqui hay de todo neng

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -8,11 +8,14 @@ import foods from '../foods'
 })
 export class FoodListComponent implements OnInit {
   foods: Object[]
-  newFoods: Object = {}
-  todayFoods: Object[] = []
+  newFoods: Object = {
+  quantity: 0
+  }
+  todayFoods: any[] = [];
   newFoodToday: Object = {}
   createFood: boolean = false
   totalCalories: Number = 0
+  todayFoodsNames: Array<string> = []
 
   constructor() { }
 
@@ -25,18 +28,31 @@ export class FoodListComponent implements OnInit {
   }
 
   addFoodToList(food) {
-    this.todayFoods.push(food)
-    this.calculateCalories(this.todayFoods)
+    // for(let i = 0; i < this.todayFoods.length; i++) {
+    //   if (food.name == this.todayFoods[i].name) {
+    //     this.todayFoods[i].quantity++
+    //   } else {
+    //     this.todayFoods.push(food)
+    //   }
+    // }
+    if(this.todayFoodsNames.indexOf(food.name) !== -1) {
+      this.todayFoods[this.todayFoodsNames.indexOf(food.name)].quantity++;
+    } else {
+      food.quantity++;
+      this.todayFoods.push(food)
+      this.todayFoodsNames.push(food.name)
+    }
+    this.totalCalories = this.calculateCalories(this.todayFoods);
   }
 
   calculateCalories(todayFoods) {
     let counter = 0
     for (let i = 0; i < this.todayFoods.length; i++) {
-      let cal = todayFoods[i].calories
+      let cal = todayFoods[i].calories * todayFoods[i].quantity;
       counter += cal
     }
     this.totalCalories = counter
-    return counter
+    return counter;
   }
 
 }
