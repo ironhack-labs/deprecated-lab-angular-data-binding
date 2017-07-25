76ff1cfed350735abdd031802530ad0a867c9cb9
Juan Macias <yo@soyjuanmacias.com>
2017-07-25 18:34:39 +0200

    1, 2 and 3 iteration ready

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import foods from '../foods';
+import { Component, OnInit } from '@angular/core'
+import foods from '../foods'
 
 @Component({
   selector: 'app-food-list',
@@ -7,10 +7,31 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
+  foods: Object[]
+  newFoods: Object = {}
+  todayFoods: Object[]
+  newFoodToday: Object = {}
+  createFood: boolean = false
 
   constructor() { }
 
   ngOnInit() {
+  this.foods = foods
+  }
+
+  showForm() {
+    this.createFood = !this.createFood
+  }
+
+  addFood() {
+    this.foods.push(this.newFoods)
+    this.newFoods = {}
+  }
+
+  addFoodToList() {
+    this.todayFoods.push(this.newFoodToday)
+    console.log(this.newFoodToday)
+    this.newFoodToday = {}
   }
 
 }
