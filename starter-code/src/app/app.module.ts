51b9b84b8882f1106f0cdaad10825697f092663f
Jordi Piella <jordipiella@gmail.com>
2017-09-19 21:27:56 +0200

    it-3

--- starter-code/src/app/app.module.ts
+++ starter-code/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { FoodListComponent } from './food-list/food-list.component';
+import { FilterPipe } from './pipes/filter.pipe';
+import { FilterComponent } from './filter/filter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FoodListComponent,
+    FilterPipe,
+    FilterComponent,
   ],
   imports: [
     BrowserModule,
--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -7,10 +7,27 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
+  foods: Array<any> = foods;
+  search: string;
+  status:boolean = false;
+  newName : string;
+  newImage : string;
+  newCalories : string;
 
   constructor() { }
 
   ngOnInit() {
+  }
+  toggleFrom(): void {
+    this.status = !this.status;
+  }
+  addFood():void{
+    let newFood : any =  {
+      name: this.newName,
+      calories: this.newImage,
+      image: this.newCalories
+    }
+    foods.push(newFood);
   }
 
 }
