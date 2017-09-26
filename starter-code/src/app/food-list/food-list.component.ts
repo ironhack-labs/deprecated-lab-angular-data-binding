6c557da310d32fff230a1f0b8c88ebc7d9375ed2
Christian Estepa <chrisestepa@MacBook-Air-de-Christian.local>
2017-09-26 17:59:11 +0200

    tercera iteraccion ok

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -4,20 +4,25 @@ import foods from '../foods';
 @Component({
   selector: 'app-food-list',
   templateUrl: './food-list.component.html',
-  styleUrls: ['./food-list.component.css']
+  styleUrls: ['./food-list.component.css'],
 })
 export class FoodListComponent {
   foodList: Array<Object> = foods;
   newFood: Object = {};
+
+  createFood(formStatus) {
+    if (formStatus.style.display == "block") {
+      formStatus.style.display = "none";
+    }
+    else {
+      formStatus.style.display = "block";
+    }
   }
 
-createFood(formStatus){
-  if (formStatus.style.display == "block"){
-    formStatus.style.display = "none"
+  addFood(formStatus) {
+    this.foodList.push(this.newFood);
+    this.createFood(formStatus);
   }
-  else {formStatus.style.display = "block"}
-}
 
-  foodList.push()
 
 }
