00eb645a371a4d4ec35fcdf3360866634cb5835f
Lucas <lfbodereau@gmail.com>
2017-09-26 16:28:26 +0200

    1 and 2

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -6,11 +6,22 @@ import foods from '../foods';
   templateUrl: './food-list.component.html',
   styleUrls: ['./food-list.component.css']
 })
+
 export class FoodListComponent implements OnInit {
+  foods: Object[];
+  newFood: Object = {};
 
-  constructor() { }
 
-  ngOnInit() {
+  constructor() {
+    this.foods = foods;
   }
 
+  ngOnInit() {
+    this.foods = foods;
+  }
+
+  addFood() {
+    this.foods.push(this.newFood);
+    this.newFood = {};
+    }
 }
