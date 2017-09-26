20c1cd3b6d40ba7e910dc41af822e7599fe3aa74
Carlos Cabezas <carloscabezascontra@gmail.com>
2017-09-26 16:12:37 +0200

    iteration 4 done

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -9,10 +9,22 @@ import foods from '../foods';
 export class FoodListComponent implements OnInit {
   foods: Array<Object> = foods;
   newFood: Object = {};
+  todayFood: Array<Object> = [];
+  calories: Number = 0;
+
+  isInputDisabled: Boolean = false;
+  toggleForm() {
+    this.isInputDisabled = !this.isInputDisabled;
+  }
 
   addFood() {
     this.foods.unshift(this.newFood);
     this.newFood = {};
+  }
+
+  addTodayFood(food) {
+    this.todayFood.push(food);
+    this.calories += food.calories;
   }
 
   constructor() { }
