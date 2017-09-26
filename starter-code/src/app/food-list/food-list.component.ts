92dbc7d5e13f47dce3f5765d25d8d8cfd314792d
Mercedes Egido <meegido@gmail.com>
2017-09-26 17:59:31 +0200

    add new item

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -6,12 +6,21 @@ import foods from '../foods';
   templateUrl: './food-list.component.html',
   styleUrls: ['./food-list.component.css']
 })
+
 export class FoodListComponent implements OnInit {
   foodlist: Array<Object> = foods
-  
+  newFood: Object = {
+
+  }
+
   constructor() { }
 
+  addItem(newFood) {
+    this.foodlist.push(this.newFood);
+  }
+
   ngOnInit() {
+    this.foodlist = foods
   }
 
 }
