0414a068aa2f3e09e47fc3a80a8aff5cbc1bcd53
Jimemart <jimena.mg@hotmail.com>
2017-09-26 17:26:29 +0200

    Done

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -7,9 +7,31 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
+  myFoods = foods
+  newFood = {}
+  todaysFood = []
+  totalCalories = 0
+  constructor() {
+  }
+  showForm(elem){
+    elem.classList.remove("hidden")
+  }
+  addFood(){
+    foods.unshift(this.newFood)
+  }
+  todayFood(food, calories){
+    this.todaysFood.push(food)
+    this.totalCalories += calories
+  
+  }
+  setQuantity(qt, food, calories){
+    if(qt === 0) qt = 1
+    food.quantity = qt
+    calories *= 3
+    this.todayFood(food, calories)
 
-  constructor() { }
 
+  }
   ngOnInit() {
   }
 
