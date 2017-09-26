bba19888a33dc32b1ebf4fba60181a638e1abd10
trigoporres <santiagotrigoporres@gmail.com>
2017-09-26 15:04:36 +0200

    primera iteracion

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -7,10 +7,11 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
-
+  food: Object[]
   constructor() { }
 
   ngOnInit() {
+    this.food = foods;
   }
 
 }
