0139eaded93600d84dfb1826a0d7eca7de451d52
pablo fuentes <pablofuentesg1@yahoo.es>
2017-09-26 15:44:58 +0200

    it 1

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -7,8 +7,10 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
+foods= foods;
 
-  constructor() { }
+
+constructor() { }
 
   ngOnInit() {
   }
