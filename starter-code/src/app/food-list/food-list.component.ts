3c6de99255cc49f0d88aec001e7952636a7b8ad0
paolo <pablocardini5@gmail.com>
2017-09-26 18:19:18 +0200

    done 4 iteration brouskissssss

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -6,11 +6,39 @@ import foods from '../foods';
   templateUrl: './food-list.component.html',
   styleUrls: ['./food-list.component.css']
 })
+
 export class FoodListComponent implements OnInit {
+  foods: Array<Object>
+  newFood : Object
+  listFood: Array<Object>
+  calories=0
+  quantity=1
+  isTurnedOn : boolean = false
 
-  constructor() { }
+  constructor() {
+    this.newFood = {}
+    this.foods = foods
+    this.listFood = []
 
-  ngOnInit() {
+
   }
 
+  ngOnInit() {}
+
+  addFood(){
+    this.foods.push(this.newFood)
+    console.log("works")
+
+  }
+
+  addFoodList(food){
+    this.listFood.push(food);
+    this.calories += food.calories;
+    this.calories = this.quantity * this.calories
+    console.log("dajeeeee")
+  }
+
+  showForm(){
+      this.isTurnedOn = !this.isTurnedOn
+  }
 }
