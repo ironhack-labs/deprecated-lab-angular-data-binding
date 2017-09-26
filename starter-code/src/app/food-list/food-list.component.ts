5326b8786b9a6469e248202c920f8b60ebb7dd07
alvar8 <alvarfernandez92@hotmail.com>
2017-09-26 18:29:17 +0200

    done

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -1,16 +1,58 @@
 import { Component, OnInit } from '@angular/core';
 import foods from '../foods';
 
+
 @Component({
   selector: 'app-food-list',
   templateUrl: './food-list.component.html',
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
-
+  foods: Object[];
+  newfood: Object = {};
+  todayFoods: Array<Object> = [];
+  //todayFood: Object ={};
+  isFormVisible : boolean =false;
+  totalCalories: number =0;
   constructor() { }
 
   ngOnInit() {
+    this.foods = foods;
   }
 
+  showForm(){
+    this.isFormVisible = !this.isFormVisible;
+  }
+
+  createElement(){
+      this.foods.push(this.newfood);
+  }
+
+  addToTodayList(food, newquantity, todayfood, calories){
+    if (typeof this.todayFoods[0] == 'undefined') {
+      food.quantity+=newquantity;
+      this.todayFoods.push(todayfood)
+      this.totalCalories+=calories;
+    }else{
+    for (var i = 0; i < this.todayFoods.length; i++) {
+      if (typeof this.todayFoods[0] == 'undefined') {
+        food.quantity+=newquantity;
+        this.todayFoods.push(todayfood)
+        this.totalCalories+=calories;}
+        else if(this.todayFoods[i] === food) {
+            food.quantity+=newquantity;
+        }else{
+            food.quantity+=newquantity;
+            this.todayFoods.push(todayfood)
+            this.totalCalories+=calories;
+        }
+      }
+
+}
+
+  }
+
+  addQuantity(food, newquantity){
+    food.quantity+=newquantity;
+  }
 }
