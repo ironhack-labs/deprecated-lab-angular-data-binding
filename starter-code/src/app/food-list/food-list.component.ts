b93c1de2ae85f6705543dc87afb675237897e837
Christian Estepa <chrisestepa@MacBook-Air-de-Christian.local>
2017-09-26 15:41:40 +0200

    primera iteraccion oK

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -7,7 +7,7 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
-
+  foodList: Array<Object> = foods;
   constructor() { }
 
   ngOnInit() {
