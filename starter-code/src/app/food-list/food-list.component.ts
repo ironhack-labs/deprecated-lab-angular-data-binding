79a287452209041d7b894bdd0d1a56cac2421d0d
allanac <acole006@fiu.edu>
2017-09-26 19:56:32 -0400

    done

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -8,9 +8,50 @@ import foods from '../foods';
 })
 export class FoodListComponent implements OnInit {
 
+    foodList:any[] = foods;
+    newFood: any = {};
+    newList: any = [];
+
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addFood(){
+    console.log('Add food has been called');
+
+    let newFood = {
+      name: this.newFood.name,
+      image: this.newFood.image,
+      calories: this.newFood.calories,
+      quantity: 0
+    }
+
+    this.foodList.push(this.newFood);
+    this.newFood = {};
+  }
+
+
+  x = true;
+  showForm() {
+
+    if (this.x === true) {
+      this.x = false;
+    }
+    else {
+      this.x = true;
+    }
+  }
+  currentCalories:number = 0;
+
+  addToList(oneFood){
+    this.currentCalories += oneFood.calories;
+
+    console.log('Calorie Count');
+    console.log(this.currentCalories);
+
+    this.newList.push(oneFood);
   }
 
 }
