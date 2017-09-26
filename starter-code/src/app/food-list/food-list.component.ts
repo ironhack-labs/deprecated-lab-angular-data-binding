0d12ae9b734df91b92fa30192521177989c042d7
Francisco Paradela <fparafer@gmail.com>
2017-09-26 18:04:29 +0200

    Third iteration done

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -7,11 +7,22 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
-  foodList: Array<Object> = foods;
+  foodList: Array<Object>;
+  newFood: Object = {};
+  toogleForm: boolean = true;
+
 
   constructor() { }
 
   ngOnInit() {
+    this.foodList = foods;
   }
 
+  addFood(newFood){
+    this.foodList.push(newFood)
+  }
+
+  showForm(){
+    this.toogleForm = !this.toogleForm
+  }
 }
