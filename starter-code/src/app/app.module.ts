8ad0a914e08b1c15991181c48137cfa1fe914179
Manuel <m.a.c.p90@gmail.com>
2017-11-13 18:39:13 -0500

    form for route done

--- starter-code/src/app/app.module.ts
+++ starter-code/src/app/app.module.ts
@@ -3,15 +3,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-
-
-
+import { Routes } from "@angular/router";
 import { AppComponent } from './app.component';
 import { FoodListComponent } from './food-list/food-list.component';
 import { FoodSearchPipe } from './pipe-search';
 import { NewFoodComponent } from './new-food/new-food.component';
 
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'new',  component: NewFoodComponent },
 
+];
 
 @NgModule({
   declarations: [
@@ -28,4 +30,6 @@ import { NewFoodComponent } from './new-food/new-food.component';
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
