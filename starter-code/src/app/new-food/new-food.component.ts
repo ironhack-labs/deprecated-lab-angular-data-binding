d2ad1ac8220dd38291dbc9ab3f239e7d5c48a7d5
Javier Buitrago <jbuitrago2014@fau.edu>
2017-11-18 02:21:14 -0500

    iteration 3 Complete and some css

---
+++ starter-code/src/app/new-food/new-food.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import foods from '../foods';
+
+@Component({
+  selector: 'app-new-food',
+  templateUrl: './new-food.component.html',
+  styleUrls: ['./new-food.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class NewFoodComponent implements OnInit {
+  foods = foods;
+  newFood: Object = {};
+
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  addFood(name, image: string, calories: number ){
+       let newFood = {
+         name: name,
+         image: image,
+         calories: calories
+       }
+
+       foods.push(newFood);
+    }
+
+
+
+
+
+
+
+}
