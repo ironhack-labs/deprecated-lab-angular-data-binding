61ad31dfccd55c9e3f050a7a27eb61584f5cc64e
Francesca Giordano <francesca020406@hotmail.com>
2017-11-23 15:01:53 -0500

    4 iterations work

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -8,9 +8,49 @@ import foods from '../foods';
 })
 export class FoodListComponent implements OnInit {
 
+  foods: Array<Object> = [];
+
+  todaysFoods: Array<Object> = [];
+
+  newFood: Object = {};
+
+  formVisible: boolean = false;
+
+  todaysCalories: number = 0;
+
   constructor() { }
 
   ngOnInit() {
+  	this.foods = foods;
+  }
+
+  calculateCalories() {
+  	this.todaysCalories = 0;
+  	var that = this;
+  	this.todaysFoods.forEach(function(currentFood:any){
+  		console.log('CURRENT FOOD ', currentFood);
+  		console.log('CURRENT FOOD CAL ', currentFood.calories);
+  		that.todaysCalories += currentFood.calories;
+  	})
+  }
+
+  addFoods(food) {
+    // this.foods.push({name: food});
+    this.foods.unshift(this.newFood);
+    this.toggleForm();
+  }
+
+  addTodaysFoods(i) {
+    console.log ("these are the foods we're trying to push", foods, i);
+    console.log ("this is the to add ", foods[i]);
+    this.todaysFoods.push(foods[i]);
+    console.log ("Todays Foods", this.todaysFoods);
+    this.calculateCalories();
+    //this.todaysFoods.push();
+  }
+
+  toggleForm() {
+    this.formVisible = !this.formVisible;
   }
 
 }
