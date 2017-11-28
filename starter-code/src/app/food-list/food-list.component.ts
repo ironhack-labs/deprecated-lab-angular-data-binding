3de470722909edeb0e3889878a8d5ec33965e06b
popelini <ccastillon92@gmail.com>
2017-11-28 19:13:10 +0100

    Iteration 3

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -7,10 +7,24 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
-  foodArray : Array<Object> = foods;
+  foodArray : Array<Object>;
+  newFood : Object = {};
+  hidden : Boolean = false;
+
   constructor() { }
 
+  toggleForm() {
+    this.hidden = !this.hidden;
+  }
+  addFood() {
+    this.foodArray.push(this.newFood);
+    this.newFood = {};
+    this.toggleForm();
+  }
+
+
   ngOnInit() {
+    this.foodArray = foods;
   }
 
 }
