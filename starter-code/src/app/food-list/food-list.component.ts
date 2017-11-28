faad5260c06d1bebe09e4558f7a332cff4183797
Asier Cisneros <ascisneros14@gmail.com>
2017-11-28 19:47:08 +0100

    4 iteration done

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -7,10 +7,38 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
+  foods: Object[];
+  newFoods: Array<Object> = [{
+    name: String,
+    calories: Number,
+    image: String,
+    quantity: Number
+  }];
+  edited = false;
+  todaysFood: Object[] = [];
+  totalCals: Number = 0;
 
   constructor() { }
 
   ngOnInit() {
+    this.foods = foods;
   }
 
+  addFoods() {
+    this.edited = !this.edited;
+    }
+
+  addNewFoods(){
+    console.log(this.newFoods);
+    console.log("Add contact has been called");
+    this.foods.unshift(this.newFoods);
+    // add contact to contacts list
+    // clear inputs
+    this.newFoods = [{}];
+  }
+  addItem(e){
+    console.log(e)
+    this.todaysFood.push(e);
+    this.totalCals += e.calories;
+  }
 }
