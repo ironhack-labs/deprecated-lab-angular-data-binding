7f84f7b8c04587cebc1e7f17ac01343e0f459878
Torgeir Beyer <torgeir.beyer@gmail.com>
2017-11-28 19:51:52 +0100

    almost done

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -7,10 +7,39 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
+ foods = foods;
+ search = null;
+ newFood: object = {};
+ dailyMenu: object[] = [];
+ displayForm = false;
+ totalCalories = 0;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  showForm():boolean {
+    console.log('hello');
+    if (this.displayForm) {
+      return this.displayForm = false;
+    }
+    else {
+      return this.displayForm = true;
+    }
+  }
+
+  addFood():void{
+    console.log('button clicked');
+    this.foods.push(this.newFood);
+    this.newFood = {};
+    this.displayForm = false;
+  }
+
+  addToMenu(i):void{
+    this.dailyMenu.push(this.foods[i]);
+    console.log(this.foods[i])
+    this.totalCalories += this.foods[i].calories;
   }
 
 }
