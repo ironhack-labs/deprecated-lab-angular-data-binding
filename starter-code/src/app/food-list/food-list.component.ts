933300cdbf01e24187a847fdd9ce1a451692d583
gemaderus <hi@gemaderus.com>
2017-11-28 15:40:03 +0100

    First iteration finished.

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -7,10 +7,30 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
+  foods: Object[];
+  newFoods: Object = {};
 
   constructor() { }
 
   ngOnInit() {
+    this.foods = foods;
   }
-
 }
+
+
+// export class ContactListComponent implements OnInit {
+//   contacts: Object[];
+//   newContact: Object = {};
+//
+//   constructor() { }
+//
+//   ngOnInit() {
+//     this.contacts = contactList;
+//   }
+//
+//   addContact(){
+//     console.log("Add contact has been called");
+//       this.contacts.push(this.newContact);
+//       this.newContact = {}
+//   }
+// }
