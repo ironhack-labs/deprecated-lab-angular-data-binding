594acdfd6ac873480dbc6755affc55db3486f5b3
Pablo Gutierrez Barcelo <pablo@gutierrezbarcelo.com>
2017-11-28 19:54:58 +0100

    iteration 1,2,3,4 + Bonus

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -10,10 +10,35 @@ export class FoodListComponent implements OnInit {
   items: Object[];
   newItem: Object = {};
   search: string;
+  show: boolean = false;
+  myList: Array<string> = [];
+  totalCalories: number = 0;
+  quantity: number = 1;
   constructor() { }
 
   ngOnInit() {
     this.items = foods;
   }
+  showList(){
+    this.show = !this.show;
+  }
 
+  addNewItem(){
+    this.items.push(this.newItem);
+  }
+l
+  addToList(i,quantity){
+    let item = this.items[i];
+    this.items[i]['quantity'] += Number(quantity);
+    this.totalCalories =0;
+    this.myList = [];
+    for (let x= 0; x < this.items.length; x++){
+      this.totalCalories += this.items[x]['calories'] * this.items[x]['quantity'];
+      if (this.items[x]['quantity'] > 1){
+        this.myList.push(this.items[x]['name']+" x"+this.items[x]['quantity']);
+      } else if (this.items[x]['quantity'] === 1) {
+        this.myList.push(this.items[x]['name']);
+      }
+    }
+  }
 }
