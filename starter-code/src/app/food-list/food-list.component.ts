d5aee502d8f6fe787e4e4de3ebdef6ec9233e7db
Anyelina Moreno Robledo <anyelinamoreno@gmail.com>
2017-11-28 20:39:17 +0100

    Iteration 4 | Today's Foods List

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -10,11 +10,28 @@ import { FilterPipe } from '../pipes/filter.pipe';
 export class FoodListComponent implements OnInit {
 
   foods: Object[];
+  newFood: Object = {}
+  seletFood: Array<object> = [];
+  countcalories: number = 0;
 
   constructor() { }
 
   ngOnInit() {
-    this.foods = foods;
+    this.foods = foods
+    for(let i = 0; i < this.foods.length; i++){
+      // this.foods[i].quantity = 1
+    }
   }
 
+  addFood() {
+    this.foods.push(this.newFood)
+  }
+
+  nextFood(food) {
+    this.seletFood.push(food)
+    this.countcalories += food.calories
+  }
+  onSubmit(f) {
+    console.log(f)
+  }
 }
