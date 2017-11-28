99cfdf6f5f3118f042e47e2f8db972e4cc2afae2
J. Gaspar Prieto <jgprieto00@gmail.com>
2017-11-28 21:24:16 +0100

    init proyect

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -2,15 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import foods from '../foods';
 
 @Component({
-  selector: 'app-food-list',
-  templateUrl: './food-list.component.html',
-  styleUrls: ['./food-list.component.css']
+  selector: "app-food-list",
+  templateUrl: "./food-list.component.html",
+  styleUrls: ["./food-list.component.css"]
 })
 export class FoodListComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
