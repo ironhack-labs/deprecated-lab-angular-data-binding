528ff18f9af7756275cc8739c688ea32c829dc5b
Alber Pedraza <alberpedrazaes@gmail.com>
2017-11-28 21:31:39 +0100

    Iteration 4 | Today's Foods List

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -9,12 +9,21 @@ import foods from '../foods';
 export class FoodListComponent implements OnInit {
   foods: Object[];
   newFood: Object = {};
+
   pattern: string;
   formActive: boolean;
+
+  addListCal: Object[];
+  NewAddListCal: object = {};
+
+  SumTotal:number;
+
   constructor() { }
 
   ngOnInit() {
+    this.addListCal = [];
     this.foods = foods;
+    this.SumTotal=0;
     this.formActive = false;
   }
   activeFor(){
@@ -26,11 +35,18 @@ export class FoodListComponent implements OnInit {
     }
   }
   addFood(){
-    console.log("Add contact has been called");
+    console.log("Add food has been called");
     console.log(this.newFood)
     this.foods.unshift(this.newFood);
     this.newFood = {};
   }
+  addFoodList(e){
+    //console.log(e);
+    this.addListCal.push(e);
+    this.SumTotal += e.calories;
+    }
+
+
   // search(ev) {
   //       console.log(`Key inserted: ${ev.key}`);
   //
