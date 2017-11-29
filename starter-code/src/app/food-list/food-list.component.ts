4a0f114afb7f3f2a77224085deded9b36faeba78
Cristina Moreno <moreno.medran@gmail.com>
2017-11-29 09:20:03 +0100

    iteration1

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -1,16 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import foods from '../foods';
+import foodsList from '../foods';
 
 @Component({
   selector: 'app-food-list',
   templateUrl: './food-list.component.html',
   styleUrls: ['./food-list.component.css']
 })
-export class FoodListComponent implements OnInit {
 
-  constructor() { }
+
+export class FoodListComponent implements OnInit {
+  foodsListed: Object[];
+
+  image: '';
+  name: '';
+  calories: number ;
+constructor() { }
 
   ngOnInit() {
+    this.foodsListed = foodsList;
+
   }
 
 }
