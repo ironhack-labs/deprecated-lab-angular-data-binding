9f213cbbbe81fe42d870e301623a4967c01a3cac
Alejandro Roman <aromanarguello@gmail.com>
2017-12-04 19:55:09 -0500

    added new forms

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -10,11 +10,22 @@ export class FoodListComponent implements OnInit {
 
   foodList: any[] = foods;
 
-
+  formConfig: any = {
+    isShowing: true
+  }
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  showForm(){
+    if(this.formConfig.isShowing) {
+       this.formConfig.isShowing = false
+    }
+    else {
+       this.formConfig.isShowing = true;
+    }
   }
 
 }
