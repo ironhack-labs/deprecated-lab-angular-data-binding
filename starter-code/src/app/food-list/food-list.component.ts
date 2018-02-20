712cde18d78eca77da7e34acf4d4cc9e8df50d30
Filipe Rainho <filipenrainho@gmail.com>
2018-02-20 20:35:06 +0100

    Almost finished

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -7,10 +7,37 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
-
-  constructor() { }
+  foods: Object[];
+  newFood: Object = {};
+  todayFood: Object[] = [];
+  showForm: Boolean = false;
+  feedbackEnabled: Boolean = false;
+  constructor() {}
 
   ngOnInit() {
+    this.foods = foods;
   }
 
+  triggerForm() {
+    if (this.showForm) {
+      this.showForm = false;
+    } else {
+      this.showForm = true;
+    }
+  }
+
+  addNewFood(form) {
+    if (form.valid) {
+      this.foods.push(this.newFood);
+      this.newFood = '';
+      this.showForm = false;
+      this.feedbackEnabled = false;
+    } else {
+      this.feedbackEnabled = true;
+    }
+  }
+
+  addTodayFood(food) {
+    this.todayFood.push(food);
+  }
 }
