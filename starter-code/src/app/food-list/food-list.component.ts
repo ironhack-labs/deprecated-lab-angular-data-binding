f99880c34b53d2f48bdd97adcf0ee70a171d3fa9
AlvaroMata8 <alvaromataaracil@hotmail.com>
2018-02-20 18:36:07 +0100

    Iteracion 3

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -7,10 +7,23 @@ import foods from '../foods';
   templateUrl: './food-list.component.html',
   styleUrls: ['./food-list.component.css']
 })
+
 export class FoodListComponent implements OnInit {
   
   foods : Array<Object> = foods;
-  
+
+  newFood : Object = {}
+
+  visible:boolean  = false;
+
+  addFood(){
+    this.visible = !this.visible;
+    this.foods.push(this.newFood)
+    this.newFood={};
+  }
+  show(){
+    this.visible = !this.visible;
+  }
   constructor() { }
 
   ngOnInit() {
