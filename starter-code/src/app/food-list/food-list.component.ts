4fbad8bba4dcfd05d6e7a335abcda659a74d2a4b
Jose Sanchez <joseansanher@gmail.com>
2018-02-20 22:08:04 +0100

    second iteration done

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -6,13 +6,14 @@ import foods from '../foods';
   templateUrl: './food-list.component.html',
   styleUrls: ['./food-list.component.css']
 })
+
 export class FoodListComponent implements OnInit {
   foods: Object[];
+  pattern: string;
 
   constructor() { }
 
   ngOnInit() {
     this.foods = foods;
   }
-
 }
