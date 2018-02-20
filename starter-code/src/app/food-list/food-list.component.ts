a0908c88d7b92943ccaeef44e1cb8c7d227af339
JoniWanKenobi <joniwankenoby@gmail.com>
2018-02-20 20:19:53 +0100

    iteration 2 done without pipes

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
+
 import foods from '../foods';
+import food from './food';
 
 @Component({
   selector: 'app-food-list',
@@ -7,12 +10,25 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
-
+  
   foods: Object[] = foods;
+  searched: string = '';
+  foodList: Object[] = foods;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  filterFoods(){   
+    if(this.searched.length>0){
+      this.foodList = this.foods.filter((food: food)=>{
+        return food.name.toLowerCase()
+                .includes(this.searched.toLowerCase());
+      }); 
+    } else {
+      this.foodList = this.foods; 
+    }
   }
 
 }
