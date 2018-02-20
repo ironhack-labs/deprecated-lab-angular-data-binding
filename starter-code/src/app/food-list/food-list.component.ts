58bf501aaf3ac910874827a47684fd3ff18bdb6a
AlvaroMata8 <alvaromataaracil@hotmail.com>
2018-02-20 17:40:12 +0100

    Iteracion 2

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import foods from '../foods';
 
+
 @Component({
   selector: 'app-food-list',
   templateUrl: './food-list.component.html',
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
+  
   foods : Array<Object> = foods;
   
   constructor() { }
