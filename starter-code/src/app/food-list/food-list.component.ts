cce1c45377ec843d133087743906b1219b7ada38
Jose Sanchez <joseansanher@gmail.com>
2018-02-20 22:22:59 +0100

    third iteration done but button not working

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -4,16 +4,21 @@ import foods from '../foods';
 @Component({
   selector: 'app-food-list',
   templateUrl: './food-list.component.html',
-  styleUrls: ['./food-list.component.css']
+  styleUrls: ['./food-list.component.css'],
 })
 
 export class FoodListComponent implements OnInit {
   foods: Object[];
   pattern: string;
+  name: Array<Object> = [];
 
   constructor() { }
 
   ngOnInit() {
     this.foods = foods;
+  }
+
+  addFood(item) {
+    this.name.push({name: item});
   }
 }
