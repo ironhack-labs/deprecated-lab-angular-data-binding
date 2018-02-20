148e991150c9a6fbe9009e07410696e0a45fb948
stepsen89 <stephanie.senoner@gmail.com>
2018-02-20 18:54:08 +0100

    iteration 2 search done

---
+++ starter-code/src/app/pipes/foodsearch.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FoodsearchPipe } from './foodsearch.pipe';
+
+describe('FoodsearchPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FoodsearchPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
