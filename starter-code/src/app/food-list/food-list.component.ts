2fc44ee0cd5db953d8992eb414fc5666ea38e5f8
Susana <susana_vadillo@yahoo.es>
2018-02-20 21:01:04 +0100

    hasta el superbonus

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -7,10 +7,30 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
-
+  foodsList: Object[];
+  pattern: string;
+  comida: Object[];
+  newFood: Object = {};
+  quantity: number;
+  totalCalories: number = 0;
+  listToday: Object[] = [];
   constructor() { }
 
   ngOnInit() {
+    this.foodsList = foods;
   }
-
+  addItem(foodName) {
+    this.foodsList.push(foodName);
+    
+    this.newFood = {
+      name:"",
+      calories:"",
+      image:""
+    }
+  }
+  addToday(comida, quantity) {
+    this.totalCalories += comida.calories * quantity;
+    comida.quantity = quantity;
+    this.listToday.push(comida);
+  }
 }
