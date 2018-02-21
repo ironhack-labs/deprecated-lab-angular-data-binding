d0b7e58a72a1f5b820f59dbf9111f5cadc961949
JoniWanKenobi <joniwankenoby@gmail.com>
2018-02-21 08:50:15 +0100

    iteration 4 done

--- starter-code/src/app/app.module.ts
+++ starter-code/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { FoodListComponent } from './food-list/food-list.component';
+import { SearchboxComponent } from './searchbox/searchbox.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FoodListComponent,
+    SearchboxComponent,
   ],
   imports: [
     BrowserModule,
--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-
 import foods from '../foods';
 import food from './food';
 
@@ -9,11 +8,24 @@ import food from './food';
   templateUrl: './food-list.component.html',
   styleUrls: ['./food-list.component.css']
 })
+
 export class FoodListComponent implements OnInit {
   
   foods: Object[] = foods;
   searched: string = '';
   foodList: Object[] = foods;
+  showForm: boolean = false;
+  requiredMsg: string = "The field is required";
+  showErrorMessages: boolean = false;
+  newFood: food = {
+    name: '',
+    calories: 0,
+    quantity: 0,
+    image: ''
+  };
+  processing: boolean = false;
+  todayList: Object[] = [];
+  totalCalories: number = 0;
 
   constructor() { }
 
@@ -29,6 +41,29 @@ export class FoodListComponent implements OnInit {
     } else {
       this.foodList = this.foods; 
     }
+  }
+
+  toggleForm(){
+    this.showForm = !this.showForm;
+  }
+
+  togglerrorMessage(){
+    this.showErrorMessages = !this.showErrorMessages;
+  }
+
+  createFood(form){
+    if(form.valid){
+      this.showErrorMessages = false;
+      this.foods.push(this.newFood);
+      this.toggleForm();
+    } else {
+      this.showErrorMessages = true;
+    }    
+  }
+
+  addToTodayList(food: food){
+    this.todayList.push(food);
+    this.totalCalories += food.calories;
   }
 
 }
