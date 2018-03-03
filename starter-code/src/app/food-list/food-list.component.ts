d7ee295dd53d5c6652fd14078c305f14dc8ca37f
Cristina Bertrand <cristina@11870.com>
2018-03-03 17:10:42 +0100

    feat:almost done

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -7,10 +7,24 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
+  foods: Array<any> = foods;
+  search: string =""
 
   constructor() { }
 
   ngOnInit() {
+    this.foods = foods;
+
   }
+
+getFoods(){
+  this.foods.filter((food) => {
+    if (this.search === "") {
+      return true;
+    } else {
+      food.name.toLowerCase().indexOf(this.search.toLowerCase()) !== -1;
+    }
+  })
+}
 
 }
