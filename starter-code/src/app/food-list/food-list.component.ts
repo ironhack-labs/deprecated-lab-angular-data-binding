ddc9cd66d3c8befd8056b3bcf75f00177a26aee7
Marco <marco238cuba@gmail.com>
2018-03-04 19:44:42 +0100

    feat: display foods

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -7,10 +7,11 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
+    foods: Array<any> = foods;
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
