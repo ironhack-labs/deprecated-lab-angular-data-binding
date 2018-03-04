8419827de1d055faf6b77d095f397aa35aa87f77
Javier Marquinez <emarquinez@gmail.com>
2018-03-04 12:57:56 +0100

    chore: filter component added, feat: Search functionality done (iteration 2)

--- starter-code/src/app/app.module.ts
+++ starter-code/src/app/app.module.ts
@@ -2,14 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
 import { FoodListComponent } from './food-list/food-list.component';
+import { FilterPipe } from './pipes/filter.pipe';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     FoodListComponent,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
