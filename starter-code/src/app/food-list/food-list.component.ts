932be5126a2512dd3dd45bd838a8dde15e5fb593
stephjackson <steph.garrett.jackson@gmail.com>
2018-03-06 13:45:34 -0500

    Done up to bonus 1

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -7,10 +7,41 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
+  foods: Object[];
+  todaysFoods: Object[] = [];
+  todaysCalories = 0;
+  newFood: Object = {};
+  buttonClicked: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
+    this.foods = foods;
   }
 
+  clickButton() {
+    this.buttonClicked = !this.buttonClicked;
+  }
+
+  addFood() {
+    this.foods.push(this.newFood);
+    this.newFood = {};
+    this.buttonClicked = false;
+  }
+
+  addToMyFood(aFood, quantity) {
+    for (var i = 0; i < quantity; i++) {
+      this.todaysFoods.push(aFood);
+    }
+
+    this.todaysCalories = this.sumCals(this.todaysFoods);
+  }
+
+  sumCals(foodArr) {
+    var cals = 0;
+    for (var i = 0; i < foodArr.length; i++) {
+      cals += foodArr[i].calories;
+    }
+    return cals;
+  }
 }
