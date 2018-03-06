3216fefd6004290832ed68591f032f48191ae551
Frieda Bulnes <friedabulnes@gmail.com>
2018-03-06 17:36:10 -0600

    terminado

---
+++ starter-code/src/app/pipes/filter.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filter'
+})
+export class FilterPipe implements PipeTransform {
+
+  transform(foods: Object[], field:string, search:string): any {
+if(!foods) return [];
+if(!search) return foods;
+const expresion = new RegExp(search, "i");
+
+return foods.filter(food=>expresion.test(food[field]));
+  }
+
+}
