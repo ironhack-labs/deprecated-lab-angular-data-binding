3d1f59887964b7211ee1cd2a4f7528d3b56243a0
Pablo Gomez <gomezpa@bellsouth.net>
2018-03-06 18:37:06 -0500

    iteration4

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -5,20 +5,26 @@ import foods from '../foods';
   selector: 'app-food-list',
   templateUrl: './food-list.component.html',
   styleUrls: ['./food-list.component.css']
-})
-export class FoodListComponent implements OnInit {
+ })
+ export class FoodListComponent implements OnInit {
   foodsArray: Object[];
   newFood: Object = {};
-
-  constructor() { }
-
+  todaysFood: Array<Object> = [];
+ 
+  constructor() {}
+ 
   ngOnInit() {
     this.foodsArray = foods;
   }
-
-  addFood(){
-    console.log("Add food has been called");
-      this.foodsArray.push(this.newFood)
-    
+  addFood() {
+    console.log('Add food has been called');
+    this.foodsArray.push(this.newFood);
+ 
+    // add contact to contacts list
+    // clear inputs
   }
-}
+  addToItemList(item) {
+    this.todaysFood.push(item);
+    console.log('my todays food list:', this.todaysFood);
+  }
+ }
