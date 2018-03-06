c1b0a6256ab1a8095ad0e7f64c5544dedd2405a8
Jonathan Sanchez <jonathansanchez@Jonathans-MacBook-Pro.local>
2018-03-06 18:01:29 -0500

    Iteration 3 Complete

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -7,10 +7,40 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
+  foods: Object[];
+  boolean: boolean;
+  newFood: Object = { };
+  newList: Array<Object>[];
 
+  buttonClickMethod() {
+    if (this.boolean == true) {
+      this.boolean = false;
+    } else {
+      this.boolean = true;
+    }
+  }
+
+  addFood(){
+    this.foods.push(this.newFood);
+    this.newFood = { };
+    // add contact to contacts list
+    // clear inputs
+    this.boolean = true;
+  }
+
+  addList(){
+    console.log(this.foods)
+  }
+
+  trackFood(index: number, food: any) {
+    console.log(food.name)
+  }
   constructor() { }
 
   ngOnInit() {
+    this.foods = foods;
+    this.boolean = true
   }
 
+  
 }
