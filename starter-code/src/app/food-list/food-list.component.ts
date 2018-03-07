8fff0b27ab2fe7ba8e1fa4c265858031bdc2a93b
DiegoArroyo <arroyodiego1@gmail.com>
2018-03-06 22:15:58 -0600

    done

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -10,6 +10,35 @@ export class FoodListComponent implements OnInit {
 
   constructor() { }
 
+  foods = foods;
+  show = false;
+  newFood = {};
+  listFoods = [];
+  numero = 0;
+  totalCalories = 0;
+
+  showFood() {
+    this.show = true;
+  }
+   addFood() {
+    this.foods.unshift(this.newFood);
+    this.show = false;
+    this.newFood = {};
+  }
+   sumFood() {
+    this.numero ++;
+  }
+  listAll(food) {
+    if (this.listFoods.find(f => f.name === food.name)) {
+      food.quantity ++;
+      this.totalCalories += food.calories;
+    } else {
+      food.quantity ++;
+      this.totalCalories += food.calories;
+      this.listFoods.push(food);
+    }
+  }
+
   ngOnInit() {
   }
 
