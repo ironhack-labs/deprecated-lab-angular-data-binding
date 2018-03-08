c85857a27ed89542ffc29d38faf661279dfae7f1
Gyasik Nunez <gyasiknunez@gmail.com>
2018-03-07 19:08:12 -0500

    done. all iterations work and added the calorie counter bonus as well -

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -2,15 +2,48 @@ import { Component, OnInit } from '@angular/core';
 import foods from '../foods';
 
 @Component({
-  selector: 'app-food-list',
-  templateUrl: './food-list.component.html',
-  styleUrls: ['./food-list.component.css']
+  selector: "app-food-list",
+  templateUrl: "./food-list.component.html",
+  styleUrls: ["./food-list.component.css"]
 })
 export class FoodListComponent implements OnInit {
+  // iteration 1 and 2
+  buttonClick: boolean = false;
 
-  constructor() { }
+  // iteration 3
+  foodList: Object[];
+  newFood: Object = {};
+
+  // iteration 4
+  todaysFoods: Array<Object> = [];
+
+  //bonus
+  calorieCount: number = 0;
+
+  constructor() {}
 
   ngOnInit() {
+    this.foodList = foods;
   }
 
+  buttonClicked() {
+    this.buttonClick = !this.buttonClick;
+    console.log(this.buttonClick);
+  }
+
+  addNewFood() {
+    console.log("Add Food Item");
+    // add food
+    this.foodList.push(this.newFood);
+    // clear
+    this.newFood = {};
+  }
+
+  addTodaysFood(addedFood) {
+    // push food object into todaysFoods array
+    this.todaysFoods.push(addedFood);
+
+    this.calorieCount += addedFood.calories;
+    console.log(this.calorieCount);
+  }
 }
