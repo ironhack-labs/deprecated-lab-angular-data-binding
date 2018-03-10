f411379b8f8cc2a5a77643355831b68a4fa09e97
Guillermo Alarcón <xxmgaxx@gmail.com>
2018-03-10 12:07:16 +0100

    feat: iteration 4

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -7,21 +7,38 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
-
   foods: Object[];
-  food: object = {};
+  food: object = {
+    name: 'Food',
+    calories: 0,
+    image: 'https://i.pinimg.com/736x/ae/8a/c2/ae8ac2fa217d23aadcc913989fcc34a2---page-empty-page.jpg'
+  };
   visible: boolean;
-
+  totalCalories: number;
+  totalFoodsCalories: Array<Object> = [];
   showForm() {
     (this.visible) ? this.visible = false : this.visible = true;
   }
 
   addFood() {
     foods.push(this.food);
+    console.log(this.food);
+    this.food = {
+      name: 'Food',
+      calories: 0,
+      image: 'https://i.pinimg.com/736x/ae/8a/c2/ae8ac2fa217d23aadcc913989fcc34a2---page-empty-page.jpg'
+    };
+    this.visible = false;
   }
 
   ngOnInit() {
     this.foods = foods;
+    this.totalCalories = 0;
   }
 
+  countCalories(food) {
+    this.totalCalories += food.quantity * food.calories;
+    this.totalFoodsCalories.push({ ...food });
+    console.log(food);
+  }
 }
