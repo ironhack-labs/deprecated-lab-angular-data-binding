cd126e8bdfb740b6338cf6d80e722b617517f502
Bart Smet <bartsmet30@gmail.com>
2018-03-11 20:15:32 +0100

    Feat: Iteration 1, listing foods done

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -8,9 +8,12 @@ import foods from '../foods';
 })
 export class FoodListComponent implements OnInit {
 
+  foods: Array<Object> = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.foods = foods;
   }
 
 }
