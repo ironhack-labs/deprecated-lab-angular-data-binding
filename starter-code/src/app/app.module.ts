0bbdf38ed1c90e1f40a8977d6994b0536fb7c92d
Noelia León <noelials@hotmail.es>
2018-03-15 20:37:21 +0100

    filter done

--- starter-code/src/app/app.module.ts
+++ starter-code/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { FoodListComponent } from './food-list/food-list.component';
 import { FooditemComponent } from './components/fooditem/fooditem.component';
+import { FilterPipe } from './pipes/filter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     FoodListComponent,
     FooditemComponent,
+    FilterPipe,
   ],
   imports: [
     BrowserModule,
