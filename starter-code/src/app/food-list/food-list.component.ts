50814c417333afe17768bfb82b41cdd7848d2fec
cosmihai <cos_mihai23@yahoo.com>
2018-05-01 22:31:59 +0200

    BCN-Mihai

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -7,10 +7,41 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
+  foodList: Object[];
+  newFood: Object = {};
+  specialFood: Array<string>;
+  showForm: boolean;
+  calCount: number;
+  qty: number;
 
   constructor() { }
 
   ngOnInit() {
+    this.foodList = foods;
+    this.showForm = false;
+    this.specialFood = [];
+    this.calCount = 0;
+    this.qty = 0;
+  }
+  addFood() {
+    this.foodList.unshift(this.newFood);
+    this.newFood = {};
+    if (this.showForm) {
+      this.showForm = false;
+    }
+  }
+  showFormAddFood() {
+    this.showForm = true;
+  }
+  addToSpecialties(food) {
+
+    this.specialFood.push(food);
+    this.calCount+=food.calories;
+    this.qty+=food.quantity;
+
+    console.log(this.specialFood);
+    console.log(this.calCount);
+
   }
 
 }
