9219c2e8e8f0f9c14a5a35866bca58740d513faf
Raul.dll <rdl.llave@gmail.com>
2018-05-01 21:01:57 +0200

    everything Done

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -8,11 +8,32 @@ import foods from '../foods';
 })
 export class FoodListComponent implements OnInit {
 foods: Array<Object>;
+platano: Boolean = false;
+favorite: Array<Object> = [];
 
   constructor() { }
 
   ngOnInit() {
     this.foods = foods;
   }
+  addFavorite(item,qty){
+    item.qty = qty.value;
+    item.total = item.qty*item.calories;
+    item.added = item.qty
+    console.log(qty)
+    if(this.favorite.indexOf(item )=== -1){
+      this.favorite.push(item)  
+    }else{
+    }
+    console.log('Add food as favorite');
+  }
+  addFood(){
+    console.log('Add food has been called');
+    this.foods.push(this.foods)
+  }
 
+  toggle(){
+    this.platano=!this.platano
+  }
 }
+
