ffa114ce21b734f1326d3b16d17f10eda4fe04e5
Raul.dll <rdl.llave@gmail.com>
2018-05-01 18:45:19 +0200

    iteration2completed

--- starter-code/src/app/app.module.ts
+++ starter-code/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { FoodListComponent } from './food-list/food-list.component';
+import { PiperanPipe } from './piperan.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     FoodListComponent,
+    PiperanPipe,
   ],
   imports: [
     BrowserModule,
