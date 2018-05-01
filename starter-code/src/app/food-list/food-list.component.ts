30b05ec45362b7ac5a2fa98b24f017db0e4091b5
aberouch <a.aberouch@gmail.com>
2018-05-01 23:53:47 +0200

    Iteration 4 done

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -9,18 +9,33 @@ import foods from '../foods';
 })
 export class FoodListComponent implements OnInit {
   foodList: Object[];
-  newFood: Object = {}
+  sideList: Object[] = [];
+  newFood: Object = {};
+  newSideListItem: Object = {};
+  totalCalories: number = 0;
 
   constructor() { }
 
   ngOnInit() {
     this.foodList = foods;
+
   }
 
 
   addToList() {
     this.foodList.push(this.newFood)
     this.newFood = {};
+  }
+
+  addToSidebar(name,calories) {
+    this.newSideListItem = {name,calories}
+    this.sideList.push(this.newSideListItem);
+    this.updateCalories(calories);
+    this.newSideListItem = {};
+  }
+
+  updateCalories(calories){
+    this.totalCalories += calories;
   }
 
 }
