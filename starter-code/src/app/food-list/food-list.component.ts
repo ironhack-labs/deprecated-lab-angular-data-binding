d9f5c4beee0ec482d043068512aa353482ee8962
Mapi Carrillo <mp.carrillo@yahoo.es>
2018-05-01 18:38:31 +0200

    Hasta el bonus, intentando que no duplique el la lista

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -7,10 +7,38 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
-
-  constructor() { }
+  foods: Object[];
+  pattern: string;
+  invisible: boolean;
+  listFood: Object[];
+  dailyCalories: number;
+  constructor() {
+    this.invisible = true;
+   }
 
   ngOnInit() {
+    this.foods = foods;
+    this.listFood = [];
+    this.dailyCalories = 0;
   }
-
+show(){
+if(this.invisible){
+  this.invisible = false
+}else{
+  this.invisible = true;
+}
+}
+addFood(name,calories, image){
+  this.foods.unshift({
+    name,
+    calories,
+    image
+  });
+ this.show()
+}
+addList(f){
+  this.listFood.push(f)
+  this.dailyCalories += (f.calories * f.quantity);
+  
+}
 }
