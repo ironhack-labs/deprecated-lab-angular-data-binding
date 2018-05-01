3ce759d9e15bbf47f1b34e221fb3b387cfc44693
Nacho Lopez <ignlopezsanchez@gmail.com>
2018-05-01 21:26:52 +0200

    done

--- starter-code/src/app/app.module.ts
+++ starter-code/src/app/app.module.ts
@@ -1,20 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { FoodListComponent } from './food-list/food-list.component';
+import { FilterPipe } from './filter.pipe';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     FoodListComponent,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -7,9 +7,60 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
+  quantity: number=1;
+  isVis: boolean = true;
+  food: Object ={
 
-  constructor() { }
+  }
+  
+  // calories: number;
+  foods: Array<object> = foods;
+  newFood: Object = {
+  };
+  specialFoods: Array<Object> =[];
+  filtrado: Array<Object> =[];
+  totalCalories:number = 0;
+  
+  constructor() { 
+  }
+  displayForm(){
+    this.isVis = !this.isVis;
+  }
+  addNewFood(){
 
+    this.foods.push(this.food)
+    this.food = {};
+  }
+  addSpecialFood(food, quantity){
+    this.totalCalories += food.calories*quantity;
+    this.newFood = {
+      name: food.name,
+      calories: parseInt(food.calories*quantity),
+      image: food.image,
+      quantity: parseInt(quantity), 
+    }
+    this.filtrado = this.specialFoods.filter((e, i)=>{
+      return e.name === food.name;
+    })
+
+    if (this.filtrado.length === 0){
+      this.specialFoods.push(this.newFood);
+
+    }
+    else{
+      this.specialFoods.map((e)=>{
+        if (e.name === food.name) {
+          e.quantity += parseInt(this.newFood.quantity);
+          e.calories += parseInt(this.newFood.calories);
+
+        }
+        return e;
+      })
+    }
+    
+
+
+  }
   ngOnInit() {
   }
 
