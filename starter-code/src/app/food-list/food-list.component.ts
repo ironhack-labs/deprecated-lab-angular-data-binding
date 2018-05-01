ec38c7242159aced559d6660cb70205616b365b3
Pere Serra Bosch <pereserrabosch@MacBook-Pro-de-Pere.local>
2018-05-01 19:56:29 +0200

    done

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import foods from '../foods';
+import foodsList from '../foods';
 
 @Component({
   selector: 'app-food-list',
@@ -7,10 +7,43 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
+  foods: Object[];
+  newFood: Object = {};
+  showForm: false;
+  todayMeal: Object[] = [];
+  count: number = 0;
 
   constructor() { }
 
   ngOnInit() {
+    this.foods = foodsList;
   }
+
+  addFood(){
+    console.log("Add food has been called");
+    this.foods.push(this.newFood)
+    // clear inputs
+    this.newFood = {};
+    // don't show form
+    this.showForm = false;
+  }
+
+  addTodayFood(food, index){
+    console.log("Add today food has been called");
+    // add quantity
+    food.quantity++;
+    // special list
+    if (food.quantity == 1){
+      this.todayMeal.push(food);
+    }
+    // this.todayMeal[index] = (food)
+    console.log(this.todayMeal)
+    // count calories
+    this.count += food.calories;
+    
+
+  }
+
+  
 
 }
