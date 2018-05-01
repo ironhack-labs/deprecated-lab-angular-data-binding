b9605d6c2627b24efbd6fa267e0aaf008d358345
lenniez <lenniezhu@gmail.com>
2018-05-02 00:20:18 +0200

    completed everything but iteration 2 and bonus so far

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -7,10 +7,41 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
+  foods: object[];
+  displayAddForm: boolean = false;
+  newFood: object = {};
+  todaysFood: object[] = [];
+  todayCalSum: number = 0;
 
   constructor() { }
 
   ngOnInit() {
+    this.foods = foods;
+
+  }
+
+  addNewFoodClick() {
+    // toggle showing the add new food form
+    this.displayAddForm = !this.displayAddForm;
+  }
+
+  submitNewFoodClick() {
+    // push newFood into food array
+    foods.push(this.newFood);
+
+    //clear inputs
+    this.newFood = {};
+
+    // toggle showing the add food form
+    this.displayAddForm = !this.displayAddForm;
+  }
+
+  addtoTodaysFood(foodItem) {
+    this.todaysFood.push(foodItem);
+    console.log(this.todaysFood);
+    
+    // sum up calories
+    this.todayCalSum += foodItem.calories;
   }
 
 }
