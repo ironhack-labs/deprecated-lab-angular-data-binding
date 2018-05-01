2b167a2a9558119b9a687044a472a74af1313a83
Rapha Montenegro <raphaelmmontenegro@gmail.com>
2018-05-01 20:23:21 +0200

    coudn't do super bonus

---
+++ starter-code/src/app/pipes/handle-sum.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { HandleSumPipe } from './handle-sum.pipe';
+
+describe('HandleSumPipe', () => {
+  it('create an instance', () => {
+    const pipe = new HandleSumPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
