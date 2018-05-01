6d5ab36624f530547365bc895c57051b21a2b1f2
Pablo García <heypablete@gmail.com>
2018-05-01 18:35:29 +0200

    iteracion 2 done

--- starter-code/src/app/app.module.ts
+++ starter-code/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { FoodListComponent } from './food-list/food-list.component';
+import { FilterByFoodnamePipe } from './filter-by-foodname.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     FoodListComponent,
+    FilterByFoodnamePipe
   ],
   imports: [
     BrowserModule,
