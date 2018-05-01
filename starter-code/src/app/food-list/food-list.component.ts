cd74056a5ac0c48d0dc5ea24e9e58211d68a1a05
FernandoGalende <fernandogalende@gmail.com>
2018-05-01 20:38:53 +0200

    bonus 1 done

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -8,10 +8,51 @@ import foods from '../foods';
 })
 export class FoodListComponent implements OnInit {
   foods:Array<Object>;
+  name:string;
+  calories:string;
+  image:string;
+  quantity:string;
+  show: Boolean = false;
+  totalCalorias: number;
+  todaysFoods:Array<Object>;
+
+
   constructor() { }
   
   ngOnInit() {
-    this.foods = foods; 
+    this.foods = foods;
+    this.todaysFoods = [] 
+    this.totalCalorias = 1;
+  }
+  addNewFood(){    
+     var food = {
+      name: this.name,
+      calories: this.calories,
+      image: this.image,
+      quantity: this.quantity
+     }
+     
+
+      this.foods.push(food)
+       
+
+      console.log(this.name)
+      /* image: this.image,
+      quantity: this.quantity, */
   }
 
+  toogle(){
+   this.show ? (this.show = false):(this.show=true)
+  }
+
+  todaysFood(e, quantity){
+    var todaysFood = {
+      name: e.name,
+      calories: e.calories,
+      image: e.image,
+      quantity: e.quantity
+     }
+     this.todaysFoods.push(todaysFood)
+     this.totalCalorias += e.calories*quantity;
+  }
 }
