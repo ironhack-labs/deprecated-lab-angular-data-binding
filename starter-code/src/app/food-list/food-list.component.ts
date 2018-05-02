d709f3348663e8f4d3ad679f6628394d3345177f
Tanis Pan <e.pan.gar@gmail.com>
2018-05-02 09:30:35 +0200

    Iteration 3

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -8,12 +8,25 @@ import foods from '../foods';
 })
 export class FoodListComponent implements OnInit {
   foods: Object[];
+  today: Object[];
   newFood: Object = {};
+  todayFood: Object = {};
 
   constructor() { }
 
   ngOnInit() {
     this.foods = foods;
+    
     }
+  
+  addFood(newFood){
+    console.log(newFood);
+    this.foods.push(newFood)
+  }
+  addToToday(todayFood){
+    console.log(todayFood);
+    this.today.push(todayFood);
+    console.log(this.today)
+  }
 
 }
