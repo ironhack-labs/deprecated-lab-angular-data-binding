8d0abd6a52db68645c4884374e4783e7acfd21a2
JMBarcelo <jmbbazal@gmail.com>
2018-05-02 09:59:49 +0200

    Un poco más apañao

--- starter-code/src/app/search-pipe.pipe.ts
+++ starter-code/src/app/search-pipe.pipe.ts
@@ -6,23 +6,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 export class SearchPipePipe implements PipeTransform {
 
-  // transform(value: any, text: string): Object[] {
-  //   console.log(value, text)
-  //   return value.filter(food => food.name.toLowerCase().includes(text));
-  // }
-
-  transform(items: any[], field: string, value: string): any[] {
-    console.log(items, field, value)
-    if (!items) {
-      return [];
-    }
-  
-    if (!value) {
-      return items;
-    }
-  
-    const myPattern = new RegExp(value, 'i');
-    return items.filter(it => it[field].match(myPattern));
+  transform(value: any, text: string): Object[] {
+    console.log(value, text)
+    if(!text) return value;
+    return value.filter(food => food.name.toLowerCase().includes(text));
   }
 }
 
