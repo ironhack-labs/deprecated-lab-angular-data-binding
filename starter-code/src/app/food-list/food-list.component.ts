a1bd8020c0a250bc58bd7c85bbcfe5b1a4c912a6
Elise Griset <elise.griset@gmail.com>
2018-05-03 16:35:17 +0200

    done

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -1,16 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import foods from '../foods';
+import {Component, OnInit} from '@angular/core';
+import {Food, foods} from '../foods';
 
 @Component({
   selector: 'app-food-list',
   templateUrl: './food-list.component.html',
-  styleUrls: ['./food-list.component.css']
+  styleUrls: ['./food-list.component.css'],
 })
 export class FoodListComponent implements OnInit {
+  foods: Array<Food> = foods;
+  userInput: string;
+  classState: any = {
+    displayForm: true,
+  };
+  newFood: any = {};
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.foods = foods;
   }
 
+  toggleClass() {
+    this.classState.displayForm = !this.classState.displayForm;
+  }
+  addFood() {
+    foods.push(this.newFood);
+    this.newFood = '';
+  }
 }
--- starter-code/src/app/foods.ts
+++ starter-code/src/app/foods.ts
@@ -1,100 +1,109 @@
-const foods: Object[] = [
+export class Food {
+  constructor(
+    public name: string,
+    public calories: number,
+    public image: string,
+    public quantity: number
+  ) {}
+}
+
+export const foods: Array<Food> = [
   {
-    name: "Pizza",
+    name: 'Pizza',
     calories: 400,
-    image: "https://i.imgur.com/eTmWoAN.png",
-    quantity: 0
+    image: 'https://i.imgur.com/eTmWoAN.png',
+    quantity: 0,
   },
   {
-    name: "Salad",
+    name: 'Salad',
     calories: 150,
-    image: "https://i.imgur.com/DupGBz5.jpg",
-    quantity: 0
+    image: 'https://i.imgur.com/DupGBz5.jpg',
+    quantity: 0,
   },
   {
-    name: "Sweet Potato",
+    name: 'Sweet Potato',
     calories: 120,
-    image: "https://i.imgur.com/hGraGyR.jpg",
-    quantity: 0
+    image: 'https://i.imgur.com/hGraGyR.jpg',
+    quantity: 0,
   },
   {
-    name: "Gnocchi",
+    name: 'Gnocchi',
     calories: 500,
-    image: "https://i.imgur.com/93ekwW0.jpg",
-    quantity: 0
+    image: 'https://i.imgur.com/93ekwW0.jpg',
+    quantity: 0,
   },
   {
-    name: "Pot Roast",
+    name: 'Pot Roast',
     calories: 350,
-    image: "https://i.imgur.com/WCzJDWz.jpg",
-    quantity: 0
+    image: 'https://i.imgur.com/WCzJDWz.jpg',
+    quantity: 0,
   },
   {
-    name: "Lasagna",
+    name: 'Lasagna',
     calories: 750,
-    image: "https://i.imgur.com/ClxOafl.jpg",
-    quantity: 0
+    image: 'https://i.imgur.com/ClxOafl.jpg',
+    quantity: 0,
   },
   {
-    name: "Hamburger",
+    name: 'Hamburger',
     calories: 400,
-    image: "https://i.imgur.com/LoG39wK.jpg",
-    quantity: 0
+    image: 'https://i.imgur.com/LoG39wK.jpg',
+    quantity: 0,
   },
   {
-    name: "Pad Thai",
+    name: 'Pad Thai',
     calories: 475,
-    image: "https://i.imgur.com/5ktcSzF.jpg",
-    quantity: 0
+    image: 'https://i.imgur.com/5ktcSzF.jpg',
+    quantity: 0,
   },
   {
-    name: "Almonds",
+    name: 'Almonds',
     calories: 75,
-    image: "https://i.imgur.com/JRp4Ksx.jpg",
-    quantity: 0
+    image: 'https://i.imgur.com/JRp4Ksx.jpg',
+    quantity: 0,
   },
   {
-    name: "Bacon",
+    name: 'Bacon',
     calories: 175,
-    image: "https://i.imgur.com/7GlqDsG.jpg",
-    quantity: 0
+    image: 'https://i.imgur.com/7GlqDsG.jpg',
+    quantity: 0,
   },
   {
-    name: "Hot Dog",
+    name: 'Hot Dog',
     calories: 275,
-    image: "https://i.imgur.com/QqVHdRu.jpg",
-    quantity: 0
+    image: 'https://i.imgur.com/QqVHdRu.jpg',
+    quantity: 0,
   },
   {
-    name: "Chocolate Cake",
+    name: 'Chocolate Cake',
     calories: 490,
-    image: "https://i.imgur.com/yrgzA9x.jpg",
-    quantity: 0
+    image: 'https://i.imgur.com/yrgzA9x.jpg',
+    quantity: 0,
   },
   {
-    name: "Wheat Bread",
+    name: 'Wheat Bread',
     calories: 175,
-    image: "https://i.imgur.com/TsWzMfM.jpg",
-    quantity: 0
+    image: 'https://i.imgur.com/TsWzMfM.jpg',
+    quantity: 0,
   },
   {
-    name: "Orange",
+    name: 'Orange',
     calories: 85,
-    image: "https://i.imgur.com/abKGOcv.jpg",
-    quantity: 0
+    image: 'https://i.imgur.com/abKGOcv.jpg',
+    quantity: 0,
   },
   {
-    name: "Banana",
+    name: 'Banana',
     calories: 175,
-    image: "https://i.imgur.com/BMdJhu5.jpg",
-    quantity: 0
+    image: 'https://i.imgur.com/BMdJhu5.jpg',
+    quantity: 0,
   },
   {
-    name: "Yogurt",
+    name: 'Yogurt',
     calories: 125,
-    image: "https://i.imgur.com/URhdrAm.png",
-    quantity: 0
-  }
-]
+    image: 'https://i.imgur.com/URhdrAm.png',
+    quantity: 0,
+  },
+];
 
 export default foods;
