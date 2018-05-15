202239f03647183cb5e27d7bf8ddd6c3c26460d2
Diego Leal <diegoleallinux@gmail.com>
2018-05-15 18:16:23 -0400

    finished with all iterations but the bonus

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -7,10 +7,54 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
+  foods: Array<any> = [];
+  foodsResults: Array<any> = foods;
+  myFoodArr = [];
+  newFoods = {name: '', calories: '', image: '', quantity: ''};
+  myFood = {name: '', calories: '', image: '', quantity: ''};
+  clicked = false;
+
+  search(searchText) {
+    this.foodsResults = this.foods.filter(food => {
+        return food.name.includes(searchText);
+      });
+  }
+
+  newFoodForm() {
+    this.clicked = true;
+  }
+
+  submit() {
+    const newOne = {name: this.newFoods.name,
+       image: this.newFoods.image,
+       calories: this.newFoods.calories,
+       quantity: this.newFoods.quantity };
+    this.foods.push(newOne);
+    this.newFoods.name = '';
+    this.newFoods.image = '';
+    this.newFoods.calories = '';
+    this.newFoods.quantity = '';
+    this.clicked = false;
+  }
+
+  myFoods(food) {
+    const oneFood = {name: food.name, image: food.image, calories: food.calories, quantity: food.quantity};
+    this.myFoodArr.push(oneFood);
+    console.log('added: ', this.myFoodArr);
+  }
+
+  caloriesCount() {
+    let totalCalories = 0;
+    this.myFoodArr.forEach(food => {
+      totalCalories += food.calories;
+    })
+    return totalCalories;
+  }
 
   constructor() { }
 
   ngOnInit() {
+    this.foods = foods;
   }
 
 }
