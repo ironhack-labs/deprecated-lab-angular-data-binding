1cc2614a9af65b5b68d3e07f8c9d6a81f3f8b292
jose arjona <josedarjona@gmail.com>
2018-05-15 19:51:57 -0400

    it works for what was asked

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import foods from '../foods';
+import foodList from '../foods';
 
 @Component({
   selector: 'app-food-list',
@@ -8,9 +8,78 @@ import foods from '../foods';
 })
 export class FoodListComponent implements OnInit {
 
-  constructor() { }
+  foods: any;
+  newFood: any = {name: '',
+        calories: '',
+        image: '',
+        quantity: ''
+    };
+
+//   todaysFood: any = {
+//         name: this.foods.name,
+//         calories: this.foods.calories,
+// }
+    todaysFood: Object[] = [];
+    todaysFoodList: any = {
+      theName: '',
+      theCalories: '',
+    };
+
+    totalCalorie: number;
+    showHide: boolean;
+    showToday: boolean;
+  constructor() {
+    this.totalCalorie = 0;
+    this.showHide = false;
+    this.showToday = false;
+  }
 
   ngOnInit() {
+    this.foods = foodList;
+  }  
+
+  changeShowStatus(){
+    this.showHide = !this.showHide;
   }
+changeTodayStatus(){
+  this.showToday = !this.showToday;
+}
+
+  addFood():void{
+    const newItem = {name: this.newFood.name,
+       calories: this.newFood.calories,
+       image: this.newFood.image,
+       quantity: this.newFood.quantity
+
+
+
+      }
+      this.newFood.name = '';
+      this.newFood.calories = '';
+      this.newFood.image = '';
+      this.newFood.quantity = '';
+    this.foods.push(newItem);
+    this.showHide = !this.showHide;
+  }
+
+  addTodaysList(name, calorie):void{
+    console.log(`the name===> ${name}`)
+    console.log(`the calorie amount===> ${calorie}`)
+    const addOne = {
+      theName: name,
+      theCalories: calorie
+    }
+
+    this.totalCalorie += calorie;
+    console.log(this.totalCalorie)
+
+    this.todaysFood.push(addOne);
+      if(!this.showToday){
+      this.showToday = !this.showToday;
+    }
+      console.log(this.todaysFood);
+
+  }
+
 
 }
