14f3ff1d2cb5a2930a125913250e894265225461
JMXXII <jeysonmmurillo@gmail.com>
2018-05-16 09:14:44 -0400

    finished

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -8,9 +8,42 @@ import foods from '../foods';
 })
 export class FoodListComponent implements OnInit {
 
+  foods: Array<any>;
+  newFood = {
+    name: '',
+    calories: '',
+    image: ''
+  };
+  foodCounter = 1;
+  todaysFood: Array<any> = [];
+  total: number = 0;
+
+
   constructor() { }
 
   ngOnInit() {
+    this.foods = foods;
   }
+
+  addFood() {
+    console.log('Chicken Bacon');
+    const newFood = {
+      name: this.newFood.name,
+      calories: this.newFood.calories,
+      image: this.newFood.image
+    };
+    this.foods.push(newFood);
+  }
+
+  addToday(name, calories){
+    const foodsToday = {
+      name: name,
+      calories: calories,
+      amount: this.foodCounter++
+    };
+    this.todaysFood.push(foodsToday);
+    this.total += calories;
+  }
+
 
 }
