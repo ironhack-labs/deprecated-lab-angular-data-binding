fd4dc30ebb97f2fa7e25b01672a1ba752fea7c83
Claudia Hernandez <hernandez.clauds@gmail.com>
2018-05-15 21:34:37 -0400

    items displayed on page - toggle not working

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import foods from '../foods';
+import foodList from '../foods';
 
 @Component({
   selector: 'app-food-list',
@@ -7,10 +7,36 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
+  foods: Object [];
+  myFoodList: any[] = foodList;
 
   constructor() { }
 
   ngOnInit() {
+    // this.foods = foodList;
   }
 
+//   $(document).ready(function(){
+//     $(`#formButton`).click(function(){
+//         $(`#form1`).toggle();
+//     });
+// });
+
+  aNewFood = { theDish: '', theCals: '', image: ''};
+
+  addFood() {
+    console.log('submit new foods has been called');
+    // add foods to list
+    const newFoods = {
+      theDish: this.aNewFood.theDish,
+      theCals: this.aNewFood.theCals,
+      image: this.aNewFood.image
+    };
+    // clear inputs
+    this.foods.unshift(newFoods);
+  }
 }
+
+
+
+
