983aacdc20fceef61f0e5b73c7d199e5b5d33f04
Brian Jaimes <jaimes.brian001@gmail.com>
2018-05-16 07:45:57 -0400

    in progress

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -7,10 +7,16 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
+  foodsList:Object[];
+  // ngOnInit is grabbing the import foods and sending to Object array[]
+  //name of object matches this. in ngOnInit
+  // newList = {name: "",calories: " ",image: " ", quantity: " "};
+ 
 
   constructor() { }
 
   ngOnInit() {
+    this.foodsList = foods;
   }
 
 }
