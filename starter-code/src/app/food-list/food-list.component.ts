874776ed5b4ac5941e0dbd4bd01879ed2de1d8a1
Jens Kai <jkaih7@gmail.com>
2018-05-15 20:18:04 -0400

    counting calories

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -8,9 +8,40 @@ import foods from '../foods';
 })
 export class FoodListComponent implements OnInit {
 
-  constructor() { }
+
+  foods: Object[];
+
+  todaysFoods: Array<any> = [];
+
+  newFoodItem =  {name: '', calories: '', image: ''};
+ 
+  total = 0;
+  
+  constructor() {
+   }
+
+  addFood() {
+    //   console.log('Added food item');
+    this.foods.push(this.newFoodItem);
+    // const newFoodItem = {name: this.newFoodItem.name, calories: this.newFoodItem.calories, image: this.newFoodItem.image };
+      // add contact to contacts list
+      // clear inputs
+    }
+  
+    selectFood(foodItem) {
+    // console.log(foodItem);
+    this.todaysFoods.push(foodItem);
+    this.total += foodItem.calories;
+    console.log(this.total);
+   
+    // const selectedFood = {name: this.selectedFood.name, calories: this.selectedFood.calories, image: this.selectedFood.image };
+  }
 
   ngOnInit() {
+    this.foods = foods;
   }
+
+
+
 
 }
