1836e1fc090efb4f2d066cd2f16c61be2cb1e943
eloji9 <eloji9@hotmail.com>
2018-07-11 19:07:17 +0200

    iteration3

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -10,10 +10,31 @@ export class FoodListComponent implements OnInit {
   foodList: Object[];
   userInput: string;
   selectedFood: string;
+  newFood: Food = new Food();
+  classState: any = {
+    hidden: true,
+  };
 
   constructor() { }
 
   ngOnInit() {
     this.foodList = foods;
   }
+
+  toggleForm() {
+    this.classState.hidden = !this.classState.hidden;
+  }
+
+  addFood() {
+    this.foodList.push(this.newFood);
+    this.newFood = new Food();
+    this.classState.hidden = !this.classState.hidden;
+  }
 }
+
+class Food{
+  name: string;
+  calories: number;
+  image: string;
+  quantity: number;
+  }
