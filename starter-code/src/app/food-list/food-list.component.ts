6d05ff65ed73a4fb58fe037fab8cf62ea4d8cbff
Miguel Rey Mallén <miguelreymallen@gmail.com>
2018-07-24 16:16:02 +0200

    Done

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -7,8 +7,45 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
-
-  constructor() { }
+  foods: Array<object> = foods;
+  addFood(input){
+    console.log(input);
+    const newFood = {
+      name: input.name,
+      calories: input.calories,
+      image: input.image
+    }
+    foods.push(newFood);
+    input.name = "";
+    input.calories = 0;
+    input.image = 0;
+    this.showForm = !this.showForm;
+  }
+  toggleForm(){
+    this.showForm = !this.showForm;
+  }
+  showForm = false;
+  newFood = {};
+  todaysFood = [];
+  finalArr = [];
+  addTodaysFood(food, quantity){
+    for (let i = 0; i < quantity; i++){
+      this.todaysFood.push(food);
+    }
+    this.finalArr = this.todaysFood.filter((item, pos) => {
+      return this.todaysFood.indexOf(item) == pos;
+    })
+    this.finalArr.forEach ( e => {
+      let counter = 0;
+      for(let i = 0; i < this.todaysFood.length; i++){
+        if(e.name === this.todaysFood[i].name){
+          counter++
+        }
+        e.numberOf = counter;
+      }
+    })
+  }
+  constructor() {}
 
   ngOnInit() {
   }
