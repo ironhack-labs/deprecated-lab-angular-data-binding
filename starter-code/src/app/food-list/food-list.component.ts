a93cdf2c5dc1e3f2044aad855f17343be9ddae6e
Pablo Rezola <pablorezola20@yahoo.es>
2018-07-24 18:01:06 +0200

    iteration3

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -8,10 +8,25 @@ import foods from '../foods';
 })
 export class FoodListComponent implements OnInit {
   foods: Object[];
+  newDish: Object = {};
+  show: boolean = false;
   constructor() { }
   
   ngOnInit() {
     this.foods = foods;
   }
+  
+  addDish(){
+    this.show = !this.show;
+    this.foods.push(this.newDish);
+    this.newDish = {
+      name: "",
+      calories: "",
+      image: ""
+    };
 
+  }
+  formMethod(){
+    this.show = !this.show;
+  }
 }
