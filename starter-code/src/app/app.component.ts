bd93a16ecc27d2bcb2a2bcd73b2cdc031b2354dd
Raúl Valenzuela <raulvalen7@hotmail.com>
2018-07-24 21:18:37 +0200

    Done

--- starter-code/src/app/app.component.ts
+++ starter-code/src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import foods from './foods';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+
 })
 export class AppComponent {
+  comida = foods;
   title = 'app works!';
+  todayFood: Object[] = [];
+  totalCalories: number = 0;
+
+  constructor() { }
+  addToday(food, quantity) {
+    food.quantity += Number(quantity)
+    if (this.todayFood.indexOf(food) == -1) {
+      this.todayFood.push(food);
+    }
+    this.totalCalories += food.calories * quantity;
+
+  }
+
 }
+
