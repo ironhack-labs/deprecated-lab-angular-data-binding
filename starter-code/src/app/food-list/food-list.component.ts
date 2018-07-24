324dab93b339212e866bc0b1d00d9de8ff9c470a
LauCF <lauracanosaf@hotmail.com>
2018-07-24 19:33:07 +0200

    DONE

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -1,16 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import foods from '../foods';
+import { Component, OnInit } from "@angular/core";
+import foods from "../foods";
 
 @Component({
-  selector: 'app-food-list',
-  templateUrl: './food-list.component.html',
-  styleUrls: ['./food-list.component.css']
+  selector: "app-food-list",
+  templateUrl: "./food-list.component.html",
+  styleUrls: ["./food-list.component.css"]
 })
 export class FoodListComponent implements OnInit {
+  foodsList: Array<object>;
+  pattern: String;
+  isHidden: Boolean = false;
+  newFood: Object = {};
+  todaysFoods: Array<Object> = []
+  foodCalories: number = 0;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.foodsList = foods;
   }
 
+  toggleHidden() {
+    this.isHidden = !this.isHidden;
+  }
+
+  ngOnInit() {}
+
+  addFood(food) {
+    this.toggleHidden();
+    console.log("Add food has been called");
+    this.foodsList.push(food);
+    this.newFood = {};
+  }
+
+  addTodaysFoods(food, q) {
+    if(!this.todaysFoods.some(e=> e['name']===food.name))this.todaysFoods.push(food);
+    food.quantity += parseInt(q);
+    this.foodCalories += food.quantity*food.calories
+  }
 }
