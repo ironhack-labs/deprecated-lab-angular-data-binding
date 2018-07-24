1a6c93bc43b88fd3441ba50323b3ea326d6f960a
Gabriel Garcia <gabogarciam71@gmail.com>
2018-07-25 01:39:25 +0200

    Iterations 1,2 and 3 done

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -7,10 +7,41 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
+  foods: Object[];
+  showAdd = false;
+  pattern: string;
+  list: [
+    {
+      name: String,
+      calories: number
+    }
+  ];
 
   constructor() { }
 
   ngOnInit() {
+    this.foods = foods;
+  }
+
+  addItem(name, calories, image) {
+    this.foods.push({
+      name: name,
+      calories: calories,
+      image: image
+    });
+    name = '';
+    calories = '';
+    image = '';
+    this.showAdd = false;
+  }
+
+  addList(name, calories) {
+    console.log(name);
+    console.log(calories);
+    this.list.push({
+      name: name,
+      calories: calories
+    });
   }
 
 }
