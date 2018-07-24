f7b603aa6d8eb86df05d1f18934431f7051a9488
Javier Romero <romeropaolini@gmail.com>
2018-07-24 21:23:33 +0200

    done

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -6,11 +6,28 @@ import foods from '../foods';
   templateUrl: './food-list.component.html',
   styleUrls: ['./food-list.component.css']
 })
+
 export class FoodListComponent implements OnInit {
+  foods: Object[];
+  newFood: Object = {};
+  todaysFood: any[] = [];
+  showForm = true;
+
+  addFood() {
+    console.log('Add food has been called');
+    this.foods.push(this.newFood);
+    this.newFood = {};
+    this.showForm = true;
+  }
+
+  handleFavouriteClick(food) {
+    this.todaysFood.push(food);
+  }
 
   constructor() { }
 
   ngOnInit() {
+    this.foods = foods;
   }
-
 }
+
