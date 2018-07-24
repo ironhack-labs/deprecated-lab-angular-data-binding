812323d7f2cd4558e6467bcb733dadca16dc2254
Leismale <leismale@gmail.com>
2018-07-24 16:05:05 +0200

    Iteration 3 done

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -1,16 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import foods from '../foods';
+import { Component, OnInit } from "@angular/core";
+import foods from "../foods";
 
 @Component({
-  selector: 'app-food-list',
-  templateUrl: './food-list.component.html',
-  styleUrls: ['./food-list.component.css']
+  selector: "app-food-list",
+  templateUrl: "./food-list.component.html",
+  styleUrls: ["./food-list.component.css"]
 })
 export class FoodListComponent implements OnInit {
+  list: Array<object>;
+  hidden: boolean = true;
+  newFood: Object = {};
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.list = foods;
   }
 
+  showForm() {
+    this.hidden = !this.hidden;
+  }
+
+  classes() {
+    return {
+      hidden: this.hidden
+    };
+  }
+
+  addFood(newFood){
+    this.list.push(newFood);
+    this.newFood = {};
+  }
 }
