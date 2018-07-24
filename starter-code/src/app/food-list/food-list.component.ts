24288e6a4a56d6c6acb312e997cfce62ba4919b1
Samuraty <alexandra.garciagasco@gmail.com>
2018-07-24 20:07:05 +0200

    Bonus done

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -1,41 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-import foods from '../foods';
+import { Component, OnInit } from "@angular/core";
+import foods from "../foods";
 
 @Component({
-  selector: 'app-food-list',
-  templateUrl: './food-list.component.html',
-  styleUrls: ['./food-list.component.css']
+  selector: "app-food-list",
+  templateUrl: "./food-list.component.html",
+  styleUrls: ["./food-list.component.css"]
 })
-
 export class FoodListComponent implements OnInit {
-  foods : Object [];
+  foods: Object[];
   pattern: string;
   newFood: any = {};
   invisible: boolean = true;
+  totalcalories: number = 0;
+
+  todaysFood: Object[] = [];
 
   constructor() {
     this.foods = foods;
-   }
-
-  ngOnInit() {
-    
   }
 
-  addFood ( name, calories, image, quantity) {
+  ngOnInit() {}
+
+  addFood(name, calories, image, quantity) {
     this.newFood.name = name;
     this.newFood.calories = calories;
     this.newFood.image = image;
     this.newFood.quantity = quantity;
 
-    this.foods.push(this.newFood)
-    this.newFood = {}
-   
+    this.foods.push(this.newFood);
+    this.newFood = {};
   }
 
   toggleVisibility() {
     this.invisible = !this.invisible;
   }
 
+  addToday(food) {
+    let {name, calories, quantity} = food;
+    quantity += 1;
+    let newDailyFood = {name, calories, quantity};
 
+    this.totalcalories += (calories * quantity);
+    this.todaysFood.push(newDailyFood);
+  }
 }
-
