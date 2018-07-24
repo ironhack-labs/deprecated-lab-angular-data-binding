05b9f0b29c8e5a559c83731b4c41df22b33accf2
dmarinsa <dmarinsanchez@gmail.com>
2018-07-24 16:21:24 +0200

    done with bonus

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -1,19 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import foods from '../foods';
+import { foodItem } from './food.interface';
 
 @Component({
   selector: 'app-food-list',
   templateUrl: './food-list.component.html',
   styleUrls: ['./food-list.component.css']
 })
-export class FoodListComponent implements OnInit {
-  foodList: Array<Object> = []
-  foodItem: Object = {}
 
-  constructor() { }
+export class FoodListComponent implements OnInit {
+  
+  foodList: Array<Object> = [];
+  todayFood: Array<Object> = [];
+  foodItem: Object = {};
+  toggleAddItem: boolean = false;
+  caloriesConsumed: number = 0;
 
   ngOnInit() {
     this.foodList = foods;
   }
 
+  toggleAdding(){
+    this.toggleAddItem = !this.toggleAddItem;
+  }
+
+  addTodayFood(item: foodItem){
+    this.caloriesConsumed += item.quantity*item.calories;
+    this.todayFood.push(item);
+  }
+
+  addItem(){
+    this.foodList.push(this.foodItem);
+    this.foodItem = {}
+    this.toggleAdding();
+  }
 }
