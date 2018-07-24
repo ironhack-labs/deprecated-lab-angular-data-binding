9bd5afd751a66b5d068e1c35a17ca81436d825da
CPBCPB <christinebeck2010@gmail.com>
2018-07-24 17:39:25 -0400

    Iteration 2 has a really good search, returns partial searches and searches with wrong capitalization.  Iteration 1 is also done.

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import foods from '../foods';
+import { AbstractFormGroupDirective } from '../../../node_modules/@angular/forms';
 
 @Component({
   selector: 'app-food-list',
@@ -7,10 +8,20 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
+foods: Array<any>;
+foods2: Array<any>;
+newFood:any = {};
 
   constructor() { }
 
   ngOnInit() {
+this.foods = foods;
+  }
+
+  findFood(searchInput){
+ this.foods2= foods.filter(function(afood){
+return afood.name.toLowerCase().includes(searchInput.value.toLowerCase());
+  })
   }
 
 }
