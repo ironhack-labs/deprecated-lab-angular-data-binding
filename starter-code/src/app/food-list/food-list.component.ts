3e51f4f61ad3c937d073e288d6ee5e5d964911b8
Julien Chil Hagopian <julienchilhagopian@gmail.com>
2018-07-25 00:03:13 +0200

    Iteration 1, 2, 3 - Done

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import foods from '../foods';
+import foodList from '../foods';
 
 @Component({
   selector: 'app-food-list',
@@ -7,10 +7,40 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
+  foods: Object[];
+  todayList = [];
+  todayFood: Object = {};
+  newFood: Object = {};
 
-  constructor() { }
+  caloriesArray = [];
+  counter: 0;
 
+
+  hideForm = false;
   ngOnInit() {
+    this.foods = foodList;
+  }
+
+  handleAddFood() {
+    console.log('pushed a new food');
+    this.foods.push(this.newFood);
+    this.hideForm = true;
+  }
+
+  handleAddtoSpecialList(itemName, itemCalories, itemImage) {
+    console.log('ok');
+    const objectToPush = {
+      name: itemName,
+      calories: itemCalories,
+      image: itemImage,
+    };
+
+      this.caloriesArray.push(itemCalories);
+      const total = this.caloriesArray.reduce((a: number, b: number) => a + b, 0);
+      this.counter = total;
+
+
+    this.todayList.push(objectToPush);
   }
 
 }
