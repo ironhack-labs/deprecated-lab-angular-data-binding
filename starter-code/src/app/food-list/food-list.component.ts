5846edb5a8de83df1bf4d2b07ac83c994d411848
Javier Martin <fjmn2c@gmail.com>
2018-07-24 23:21:39 +0200

    Falta maquetar

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -1,16 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import foods from '../foods';
+import { Component, OnInit } from "@angular/core";
+import foods from "../foods";
 
 @Component({
-  selector: 'app-food-list',
-  templateUrl: './food-list.component.html',
-  styleUrls: ['./food-list.component.css']
+  selector: "app-food-list",
+  templateUrl: "./food-list.component.html",
+  styleUrls: ["./food-list.component.css"]
 })
 export class FoodListComponent implements OnInit {
+  foodList: Object[];
+  newFood: Object = {};
+  todayFoods: Object[]=[];
+  totalCalories: number=0;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.foodList = foods;
   }
 
+  addFood() {
+    this.foodList.push(this.newFood);
+  }
+
+  addTodayFood(name,q) {
+
+    this.foodList.forEach(e => {
+      if (e["name"] === name) {
+        this.todayFoods.push(e);
+        this.totalCalories += e["calories"]*parseInt(q);
+      }
+    });
+  }
 }
