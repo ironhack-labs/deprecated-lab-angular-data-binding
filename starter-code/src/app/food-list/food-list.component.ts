2ade0f97709a0fc6489cb6650a906f7510144644
Verónica Reina <veroreinah@gmail.com>
2018-07-24 15:59:45 +0200

    Iteration 3 | Add New Foods

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -9,11 +9,30 @@ import foods from '../foods';
 export class FoodListComponent implements OnInit {
 
   foods: Array<object>;
+  newFood: Object = {};
+  showForm: boolean = false;
+  fieldsError: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
     this.foods = foods;
+  }
+
+  toggleForm() {
+    this.showForm = !this.showForm;
+  }
+
+  addFood() {
+    if (this.newFood["name"] !== "" && this.newFood["calories"] !== "" && this.newFood["image"] !== "") {
+      this.foods.push(this.newFood);
+
+      this.newFood = {};
+      this.showForm = false;
+      this.fieldsError = false;
+    } else {
+      this.fieldsError = true;
+    }
   }
 
 }
