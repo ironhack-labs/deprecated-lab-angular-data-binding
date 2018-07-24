808d38869d6ce052824881bcd6d72092e3f5bfa7
YokoYolo <y.ravlyk@gmail.com>
2018-07-24 18:05:25 -0400

    finally

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import foods from '../foods';
+import foodList from '../foods';
 
 @Component({
   selector: 'app-food-list',
@@ -7,10 +7,42 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
-
+  [x: string]: any;
+  foods: Array <any>;
+  todayfoods: Array <any> = [];
+  newFood= {};
+  newList:Array <any> = [];
+  
   constructor() { }
 
+  addFood(){
+    console.log("Add food has been called");
+    const nF = this.newFood;
+    this.foods.push(nF);
+    
+  }
+
+  addTodayFood(addedFood){
+
+    console.log(addedFood);
+    this.todayfoods.push(addedFood)
+  }
+
+
+  findFood(searchThing) {
+    
+    this.foods.forEach((eachFood) => {
+      if (eachFood.name === searchThing.value) {
+        this.newList.push(eachFood) 
+      }
+   console.log (eachFood.name)
+    })
+  
+  }
+
+
   ngOnInit() {
+  this.foods = foodList;
   }
 
 }
