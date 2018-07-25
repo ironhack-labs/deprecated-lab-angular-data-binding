c21c4b50bdd1a43a8aea5a60f3068f07b06a5ada
Rafael González Vázquez <rafaelg@lycklig.com.mx>
2018-07-24 20:55:40 -0500

    DE MEX - Rafael González

---
+++ starter-code/src/app/find-food.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FindFoodPipe } from './find-food.pipe';
+
+describe('FindFoodPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FindFoodPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -7,10 +7,57 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
+  showForm: boolean = false;
+  foodName: string = '';
+  calories: string = '';
+  image: string = '';
+  showList: boolean = false;
+  totalCal: number = 0;
+
+  foods: Object[] = foods;
+  todayFoods: any = [];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addFood(name, calories, image){
+    if(!name || !calories || !image){
+      alert('llena todos los datos');
+      return;
+    }
+
+    foods.push({
+      name,
+      calories,
+      image,
+      quantity: 0
+    });
+
+    this.foodName = '';
+    this.calories = '';
+    this.image = '';
+    this.showForm = false;
+  }
+
+  sumCalories(){
+    this.totalCal = 0;
+    this.todayFoods.forEach(food=>{
+      this.totalCal += (food.calories * food.quantity);
+    });
+  }
+
+  foodCount(comida){
+    comida.quantity = parseInt(comida.quantity);
+    if(comida.quantity > 0){
+      this.todayFoods.push(comida);
+      this.sumCalories();
+      this.showList = true;
+    } else {
+      alert('Agrega una cantidad');
+    }
+    
   }
 
 }
