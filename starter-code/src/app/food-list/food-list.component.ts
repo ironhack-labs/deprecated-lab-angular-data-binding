28a53db50182eeb9ab40a4b60a942ce0e787f1cd
Rodrigo Flores <rflor12@gmail.com>
2018-07-25 10:18:23 -0400

    adding

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -7,23 +7,35 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
-  foods: Array<any>;
-  searchedFoods:any[];
-  listOfFoods:string="";
-  newFood: Object = {};
+  foodsList: Array<any>;
+  searchedFoods: any[];
+  theSearchTerm: string = "";
+  newFoodItem:any = {}; //in the food list component reference the names, calories
+  formShowing:boolean = false;
 
   constructor() { }
 
   ngOnInit() {
-    this.foods = foods;
+    this.foodsList = foods;
     this.searchedFoods = foods;
   }
+//function that runs everytime component is loaded on the page, runs when you go to page,
 
-  searchFunction(){
-    this.searchedFoods = this.foods.filter((foodsearch)=>{
-      return foodsearch.name.toLowerCase().includes(this.listOfFoods.toLowerCase());
+  searchFunction() {
+    this.searchedFoods = this.foodsList.filter((foodsearch) => {
+      return foodsearch.name.toLowerCase().includes(this.theSearchTerm.toLowerCase());
     })
+  }
 
-}
+  addNewFood(){
+    this.foodsList.unshift(this.newFoodItem);
+    this.newFoodItem = {};
+    this.toggleForm();
+    console.log(this.newFoodItem);
+  }
+
+  toggleForm(){
+    this.formShowing = !this.formShowing;
+  }
 
 }
