4d3ebf991214c3d6ebe6f52d9c0eab67eea0496d
EdMijangos <mloeza2010@hotmail.com>
2018-07-25 08:12:30 -0500

    DE MEX - Eduardo Mijangos

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -7,10 +7,45 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
+  foodList: Object[];
+  totalCalories: number = 0;
+  show: boolean = false;
+  newFood: Object[] = [];
+  todaysFoods: Array<any> = [];
+
+  addFood(){
+    let i = 0;
+    while (this.foodList[i]){
+      if(this.foodList[i] == this.newFood) return;
+      else {
+        i++;
+        this.foodList.push(this.newFood);
+        this.newFood = [];
+        this.show = false
+      }
+    }
+  }
+
+  showForm(){
+    if(!this.show) return this.show = true;
+    return this.show = false;
+  }
+
+  addQuantity(food){
+    this.totalCalories = 0;
+    this.todaysFoods.push(food)
+    let i = 0;
+    while (this.todaysFoods[i]) {
+      this.totalCalories += (this.todaysFoods[i].calories * this.todaysFoods[i].quantity);
+      i++;
+    }
+    console.log(this.totalCalories)
+  }
 
   constructor() { }
 
   ngOnInit() {
+    this.foodList = foods
   }
 
 }
