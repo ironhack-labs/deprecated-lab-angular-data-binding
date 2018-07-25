13900f8cc17880a0056f4a5561ab95d538f81342
Junior A. Mendez M <junior_abigail@hotmail.com>
2018-07-25 17:02:28 -0400

    improved code of stringifier pipe

--- starter-code/src/app/stringifier.pipe.ts
+++ starter-code/src/app/stringifier.pipe.ts
@@ -8,74 +8,30 @@ export class StringifierPipe implements PipeTransform {
   transform(value: any, args?: any): any {
     return this.stringify(value);
   }
-  
+
   stringify(number) {
     let result = '';
-    let numberString = String(number);
-    let digits = numberString.length;
+    let inputString = String(number).split('');
+    console.log(inputString);
+    const singleDigits = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const doubleDigits = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
-    if(number === 0) {
-      return 'zero';
-    }
-  
-    while(number > 0) {
-      if(number < 20) {
-        result += toWord(number);
-        number = 0;
-      } else if(number < 100) {
-        result += toWordTens(Math.floor(number / 10)) + ' ';
-        number -= 10 * Math.floor(number / 10);
-      } else if(number < 1000) {
-        result += toWord(Number(numberString.charAt(0))) + ' hundred ';
-        numberString = numberString.slice(0, 1);
-        number -= 100 * Math.floor(number / 100);
-      } else if(number < 10000) {
-        result += toWord(Number(numberString.charAt(0))) + ' thousand ';
-        numberString = numberString.slice(1, numberString.length);
-        number -= 1000 * Math.floor(number / 1000);
-      } else {
-        return "DON'T EAT THAT MUCH! FAT A**"
+    while(inputString.length > 0) {
+      if(number < 20 || inputString.length === 1) {
+        result += singleDigits[number];
+        return result;
+      } else if(inputString.length === 2) {
+        result += doubleDigits[Number(inputString[0])] + ' ';
+        number -= Number(inputString[0]) * 10;
+      } else if(inputString.length === 3) {
+        result += singleDigits[Number(inputString[0])] + ' hundred ';
+        number -= Number(inputString[0]) * 100;
+      } else if(inputString.length === 4) {
+        result += singleDigits[Number(inputString[0])] + ' thousand ';
+        number -= Number(inputString[0]) * 1000;
       }
+      inputString = inputString.slice(1, inputString.length);
     }
-  
-    function toWord(number) {
-      switch (number) {
-        case 1: return 'one';
-        case 2: return 'two';
-        case 3: return 'three';
-        case 4: return 'four';
-        case 5: return 'five';
-        case 6: return 'six';
-        case 7: return 'seven';
-        case 8: return 'eight';
-        case 9: return 'nine';
-        case 10: return 'ten';
-        case 11: return 'eleven';
-        case 12: return 'twelve';
-        case 13: return 'thirteen';
-        case 14: return 'fourteen';
-        case 15: return 'fifteen';
-        case 16: return 'sixteen';
-        case 17: return 'seventeen';
-        case 18: return 'eighteen';
-        case 19:  return  'nineteen';
-      }
-    }
-  
-    function toWordTens(number) {
-      switch (number) {
-        case 2: return 'twenty';
-        case 3: return 'thirty';
-        case 4: return 'forty';
-        case 5: return 'fifty';
-        case 6: return 'sixty';
-        case 7: return 'seventy';
-        case 8: return 'eighty';
-        case 9: return 'ninety';
-      }
-    }
-  
     return result;
-  } 
-
+  }
 }
