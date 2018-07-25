42dda452ad992fb54176801c051c8f7fc4930e99
Felipe Vallina <fvallinalp@gmail.com>
2018-07-24 23:59:54 -0500

    done

---
+++ starter-code/src/app/filter.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filter'
+})
+export class FilterPipe implements PipeTransform {
+
+  transform(array: Array<any>, field: string, value: string, args?: any): any {
+    if (!value) {return array; }
+    const newRegx = new RegExp(value, 'i');
+   return array.filter(obj => obj[field].match(newRegx));
+  }
+}
