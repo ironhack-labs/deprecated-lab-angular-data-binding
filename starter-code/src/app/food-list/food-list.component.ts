15b91bf2d5a077ef5f2953e22f842996d8f86f84
“yannyyss” <yannyyss@hotmail.com>
2018-07-25 08:49:14 -0500

    casi terminado, problemas con la suma

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -7,10 +7,28 @@ import foods from '../foods';
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
+  foodlist = foods;
+  show: boolean;
+  form: any = {};
+  myFoods: Array<any> = [];
+  quantity: any;
+  totalCalories: number = 0;
 
-  constructor() { }
-
-  ngOnInit() {
+  todaysFoods(food) {
+    this.myFoods.push(food);
+    this.totalCalories += (food.calories * food.quantity);
   }
 
+  addNewFood() {
+    this.foodlist.unshift(this.form)
+  }
+
+  toggle() {
+    this.show = !this.show;
+  }
+
+  ngOnInit() {
+    
+  }
+  
 }
