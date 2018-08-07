baa2c2f11b6bd8d1e80044602b1706b19f673ac3
Anna Fredriksson <leliloro@gmail.com>
2018-08-07 21:18:51 +0200

    Bonus and superbonus done

--- lab-iron-nutrition-ng-data-binding/src/app/components/food-list/food-list.component.ts
+++ lab-iron-nutrition-ng-data-binding/src/app/components/food-list/food-list.component.ts
@@ -14,7 +14,6 @@ export class FoodListComponent implements OnInit {
       name: '',
       calories: '',
       image: '',
-      quantity: ''
   };
 
   visibleForm = false;
@@ -27,9 +26,20 @@ export class FoodListComponent implements OnInit {
     this.foods = foodList;
   }
 
-  addToList(i){
-    this.myFoodList.push(this.foods[i]);
-    this.cal += this.foods[i].calories;
+  addToList(index,quant){
+    let position = this.myFoodList.indexOf(this.foods[index]);
+    let quantNum = Number(quant.value);
+    
+    if(position >= 0) {
+      this.myFoodList[position].quantity += quantNum;
+      this.cal += Number(this.foods[index].calories) * quantNum;
+    }
+    else{
+      this.foods[index].quantity = quantNum;
+      this.myFoodList.push(this.foods[index]);
+      this.cal += Number(this.foods[index].calories) * quantNum;
+    }
+    
   }
 
   toggleForm(){
@@ -42,7 +52,6 @@ export class FoodListComponent implements OnInit {
       name: '',
       calories: '',
       image: '',
-      quantity: ''
     }
     this.toggleForm();
   }
