5630341388ab2bb2cef5e86dbe1959b028d0b369
Nicholas Kreps <nicholas.kreps@gmail.com>
2018-08-11 19:57:31 -0400

    iteration 3 complete

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -9,11 +9,20 @@ import foods from '../foods';
 export class FoodListComponent implements OnInit {
   foodsList: Object[];
   pattern: string;
-  
+  display: string;
+
   constructor() { }
 
   ngOnInit() {
     this.foodsList = foods;
   }
 
+  addNewFoodForm(){
+    this.display = 'true';
+  }
+
+  addFoodItem(name, calories, image) {
+    this.foodsList.push({name: name, calories: calories, image: image});
+    this.display = 'false';
+  }
 }
