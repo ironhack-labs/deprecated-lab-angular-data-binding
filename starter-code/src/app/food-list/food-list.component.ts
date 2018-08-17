3168afa0b849b0097611f9b079471538efb6d684
Yandy Gonzalez <yandygonzalez@gmail.com>
2018-08-17 09:31:06 -0400

    Filter Foods

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -1,18 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import foods from '../foods';
 
+interface foods {
+  name: string;
+  calories: number;
+  image: string;
+  quantity: number;
+}
+
 @Component({
   selector: 'app-food-list',
   templateUrl: './food-list.component.html',
   styleUrls: ['./food-list.component.css']
 })
 export class FoodListComponent implements OnInit {
-  foods: Object[];
+  foodArr: Object[];
+  pattern: string;
 
   constructor() { }
 
   ngOnInit() {
-    this.foods = foods;
+    this.foodArr = foods;
   }
 
 }
