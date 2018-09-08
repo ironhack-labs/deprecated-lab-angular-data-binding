db6868ac74d69a57c02ffba4b95be20f59d940ee
Paula Quitián <pquitian@idealista.com>
2018-09-08 17:23:11 +0200

    moving components to component directory

---
+++ starter-code/src/app/components/food-list/food-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import foods from '../../shared/data/foods.data';
+
+@Component({
+  selector: 'app-food-list',
+  templateUrl: './food-list.component.html',
+  styleUrls: ['./food-list.component.css']
+})
+export class FoodListComponent {
+
+}
