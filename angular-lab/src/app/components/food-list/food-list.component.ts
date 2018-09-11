220dfe2513e54cf098dc791632854bb2f30b88a5
Paula Quitián <pqhernandez@gmail.com>
2018-09-11 21:15:35 +0200

    feat: iteration 3 -- adding new food!

--- angular-lab/src/app/components/food-list/food-list.component.ts
+++ angular-lab/src/app/components/food-list/food-list.component.ts
@@ -1,20 +1,28 @@
 import { Component } from '@angular/core';
 import foods from '../../shared/data/foods.data';
+import { Meals } from '../../shared/models/meals.model';
 
 @Component({
   selector: 'app-food-list',
   templateUrl: './food-list.component.html'
 })
 export class FoodListComponent {
-  foods = foods;
+  foods: Array<Meals> = foods;
+  newFood: Meals = new Meals();
   showForm: boolean = false;
 
   onClickShowForm(): void {
     this.showForm = true;
   }
 
-  onClickAddFood(name, calories, image): void {
-    //Create new Food
-
+  onClickAddFood(name: string, calories: number, image: string): void {
+    if(!this.newFood.name || !this.newFood.calories || !this.newFood.image ) {
+      this.showForm = false;
+    }
+    else {
+      this.foods.push(this.newFood);
+      this.newFood = new Meals();
+      this.showForm = false;
+    }
   }
 }
