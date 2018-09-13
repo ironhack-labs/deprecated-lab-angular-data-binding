d5f946e6efd01928c71e8188b1932f692364c0d7
Alex González <alexgm@MacBook-Pro-de-Alex.local>
2018-09-13 09:49:38 +0200

    Delete ironhack files, add new files

--- lab-starter-code/src/app/app.module.ts
+++ lab-starter-code/src/app/app.module.ts
@@ -1,20 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { FoodListComponent } from './food-list/food-list.component';
+import { FoodListComponent } from './components/food-list/food-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FoodListComponent,
+    FoodListComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- lab-starter-code/src/app/components/food-list/food-list.component.ts
+++ lab-starter-code/src/app/components/food-list/food-list.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import foods from '../foods';
 
 @Component({
   selector: 'app-food-list',
