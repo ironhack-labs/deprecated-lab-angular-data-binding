8521e31ff11d192ff8afc876fd900c61f2f78b57
Daniel Ayuso <daniel.ayuso@innocv.com>
2019-04-03 23:16:14 +0200

    implement add food issue

--- starter-code/src/app/food-list/food-list.component.ts
+++ starter-code/src/app/food-list/food-list.component.ts
@@ -12,15 +12,34 @@ export class FoodListComponent implements OnInit {
   foodList: Array<FoodItem>;
   imagesPath = '/assets/images/';
   search: string;
+  isVisibleForm = false;
+  newFood: FoodItem;
 
   constructor() { }
 
   ngOnInit() {
+
+    this.newFood = { name: undefined, image: undefined, calories: undefined }
+
     this.foodList = [
       { name: 'Pizza', image: 'pizza.jpeg', calories: 827 },
       { name: 'Vegetables', image: 'vegetable.jpeg', calories: 248 },
       { name: 'Rice with chicken', image: 'rice.jpeg', calories: 472 }
     ]
+  }
+
+  openNewFoodForm() {
+    this.isVisibleForm = true;
+  }
+
+  addFood() {
+    if (this.newFood.name && this.newFood.calories && this.newFood.image &&
+       !!this.newFood.name.length && !!this.newFood.image.length && this.newFood.calories > 0) {
+      this.foodList.push(this.newFood);
+      this.isVisibleForm = false;
+    } else {
+      alert('All fields are mandatory')
+    }
   }
 
 }
